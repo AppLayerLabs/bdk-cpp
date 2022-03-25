ab1fe8edb0a6c08d8217a0bbde1fa84bbfccd1be
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2022-03-25 23:02:39 +0100

    Update main.h

--- src/main.h
+++ src/main.h
@@ -4,11 +4,16 @@
 #include <thread>
 #include <sstream>
 #include <fstream>
+#include <chrono>
 
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 #include <grpc/support/log.h>
 #include <grpcpp/grpcpp.h>
 
+
+#include <google/protobuf/text_format.h>
+#include <google/protobuf/util/json_util.h>
+#include <google/protobuf/message.h>
 #include "../proto/vm.grpc.pb.h"
 #include "libs/CommonData.h"
 
@@ -40,255 +45,207 @@ class VMServiceImplementation final : public vmproto::VM::Service {
   Status Initialize(ServerContext* context, const vmproto::InitializeRequest* request,
                   vmproto::InitializeResponse* reply) override {
     logToFile("Initialized Called");
+    std::string jsonRequest;
+    google::protobuf::util::JsonOptions options;
+    google::protobuf::util::MessageToJsonString(*request, &jsonRequest, options);
+    logToFile(jsonRequest);
+    // We only have the genesis for now, answer it
 
-    json debugJson;
-    debugJson["call"] = "Initialize";
-    json initializeRequest;
-    initializeRequest["network_id"] = boost::lexical_cast<std::string>(request->network_id());
-    initializeRequest["subnet_id"] = request->subnet_id().c_str();
-    initializeRequest["chain_id"] = request->chain_id().c_str();
-    initializeRequest["node_id"] = request->node_id().c_str();
-    initializeRequest["x_chain_id"] = request->x_chain_id().c_str();
-    initializeRequest["avax_asset_id"] = request->avax_asset_id().c_str();
-    initializeRequest["genesis_bytes"] = request->genesis_bytes().c_str();
-    initializeRequest["upgrade_bytes"] = request->upgrade_bytes().c_str();
-    initializeRequest["config_bytes"] = request->config_bytes().c_str();
-    initializeRequest["VersionedDBServer"] = json::array();
-    for (auto dbServer : request->db_servers()) {
-      json dbJson;
-      dbJson["db_server"] = boost::lexical_cast<std::string>(dbServer.db_server());
-      dbJson["version"] = dbServer.version();
+    std::string zeroStr;
+    // last_accepted_id == 32 bytes, last blockhash
+    // last_accepted_parent_id == 32 bytes, last block parent blockhash
+    for (uint64_t i = 0; i < 32; ++i) {
+      zeroStr += "0";
     }
-    initializeRequest["init_server"] = boost::lexical_cast<std::string>(request->init_server());
-    debugJson["request"] = initializeRequest;
-    logToFile(debugJson.dump(2));
+    logToFile(zeroStr);
+    logToFile(boost::lexical_cast<std::string>(sizeof(zeroStr)));
+    reply->set_last_accepted_id(zeroStr);
+    reply->set_last_accepted_parent_id(zeroStr);
+    reply->set_status(1);
+    reply->set_height(1);
+    // bytes -> last block bytes.
+    reply->set_bytes(std::string("aaa")); 
+    const auto p1 = std::chrono::system_clock::now();
+    std::string timestampStr;
+    // HEX timestamp (bytes->hex) 15 bytes.
+    // 0x010000000ed9d0262f3b11354aff10
+    // 0x01 -- Version
+    // not sure if the below information is correct, more testing and check with the rust code needed.
+    // 00 00 00 0e d9 d0 26 2f -- Seconds
+    // 3b 11 35 4a -- Milliseconds 
+    // ff 10 -- Nanoseconds
+    // information took from here: https://github.com/archisgore/landslide/blob/main/src/timestampvm/state.rs#L383
+    // EXAMPLE
+    // t := time.Date(2001, 2, 1, 14, 30, 12, 05, time.UTC)
+    // 0x010000000eb20b69F400000005ffff
+
+    // i64 (8 bytes)
+
+    timestampStr.resize(15);
+    timestampStr[0] = 0x01;
+    timestampStr[1] = 0x00;
+    timestampStr[2] = 0x00;
+    timestampStr[3] = 0x00;
+    timestampStr[4] = 0x0e;
+    timestampStr[5] = 0xd9;
+    timestampStr[6] = 0xd0;
+    timestampStr[7] = 0x26;
+    timestampStr[8] = 0x2f;
+    timestampStr[9] = 0x3b;
+    timestampStr[10] = 0x11;
+    timestampStr[11] = 0x35;
+    timestampStr[12] = 0x4a;
+    timestampStr[13] = 0xff;
+    timestampStr[14] = 0x10;
+
+    reply->set_timestamp(timestampStr);
+    std::string jsonAnswer;
+    google::protobuf::util::MessageToJsonString(*reply, &jsonAnswer, options);
+    logToFile(jsonAnswer);
     return Status::OK;
   }
 
   Status SetState(ServerContext* context, const vmproto::SetStateRequest* request, google::protobuf::Empty* reply) override {
     logToFile("SetState called");
-    json debugJson;
-    debugJson["call"] = "SetState";
-    json setStateRequest;
-    setStateRequest["state"] = boost::lexical_cast<std::string>(request->state());
-    debugJson["request"] = setStateRequest;
-    logToFile(debugJson.dump(2));
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status Shutdown(ServerContext* context, const google::protobuf::Empty* request, google::protobuf::Empty* reply) override {
     logToFile("Shutdown Called");
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status CreateHandlers(ServerContext* context, const google::protobuf::Empty* request, vmproto::CreateHandlersResponse* reply) override {
     logToFile("CreateHandlers Called");
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status CreateStaticHandlers(ServerContext* context, const google::protobuf::Empty* request, vmproto::CreateStaticHandlersResponse* reply) override {
     logToFile("CreateStaticHandlers Called");
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status Connected(ServerContext* context, const vmproto::ConnectedRequest* request, google::protobuf::Empty* reply) override {
     logToFile("Connected Called");
-    json debugJson;
-    debugJson["call"] = "Connected";
-    json connectedRequest;
-    connectedRequest["node_id"] = request->node_id().c_str();
-    connectedRequest["version"] = request->version();
-    debugJson["request"] = connectedRequest;
-    logToFile(debugJson.dump(2));
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status Disconnected(ServerContext* context, const vmproto::DisconnectedRequest* request, google::protobuf::Empty* reply) override {
     logToFile("Disconnected Called");
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status BuildBlock(ServerContext* context, const google::protobuf::Empty* request, vmproto::BuildBlockResponse* reply) override {
     logToFile("BuildBlock called");
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status ParseBlock(ServerContext* context, const vmproto::ParseBlockRequest* request, vmproto::ParseBlockResponse* reply) override {
     logToFile("ParseBlock called");
-    json debugJson;
-    debugJson["call"] = "ParseBlock";
-    json parseBlockRequest;
-    parseBlockRequest["bytes"] = request->bytes().c_str();
-    debugJson["request"] = parseBlockRequest;
-    logToFile(debugJson.dump(2));
-
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status GetBlock(ServerContext* context, const vmproto::GetBlockRequest* request, vmproto::GetBlockResponse* reply) override {
     logToFile("GetBlock called");
-    json debugJson;
-    debugJson["call"] = "GetBlock";
-    json getBlockRequest;
-    getBlockRequest["id"] = request->id().c_str();
-    debugJson["request"] = getBlockRequest;
-    logToFile(debugJson.dump(2));
+    logToFile(request->DebugString());
     return Status::OK; 
   }
 
   Status SetPreference(ServerContext* context, const vmproto::SetPreferenceRequest* request, google::protobuf::Empty* reply) override {
     logToFile("SetPreference Called");
-    json debugJson;
-    debugJson["call"] = "SetPreference";
-    json setPreferenceRequest;
-    setPreferenceRequest["id"] = request->id().c_str();
-    debugJson["request"] = setPreferenceRequest;
-    logToFile(debugJson.dump(2));
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status Health(ServerContext* context, const google::protobuf::Empty* request, vmproto::HealthResponse* reply) override {
     logToFile("Health called");
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status Version(ServerContext* context, const google::protobuf::Empty* request, vmproto::VersionResponse* reply) override {
     logToFile("Version Called");
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status AppRequest(ServerContext* context, const vmproto::AppRequestMsg* request, google::protobuf::Empty* reply) override {
     logToFile("AppRequest called");
-    json debugJson;
-    debugJson["call"] = "AppRequest";
-    json appRequest;
-    appRequest["node_id"] = request->node_id().c_str();
-    appRequest["request_id"] = boost::lexical_cast<std::string>(request->request_id());
-    appRequest["deadline"] = request->deadline().c_str();
-    appRequest["request"] = request->request().c_str();
-    debugJson["request"] = appRequest;
-    logToFile(debugJson.dump(2));
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status AppRequestFailed(ServerContext* context, const vmproto::AppRequestFailedMsg* request, google::protobuf::Empty* reply) override {
     logToFile("AppRequestFailed called");
-    json debugJson; 
-    debugJson["call"] = "AppRequestFailed";
-    json appRequestFailedRequest;
-    appRequestFailedRequest["node_id"] = request->node_id().c_str();
-    appRequestFailedRequest["request_id"] = boost::lexical_cast<std::string>(request->request_id());
-    debugJson["request"] = appRequestFailedRequest;
-    logToFile(debugJson.dump(2));
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status AppResponse(ServerContext* context, const vmproto::AppResponseMsg* request, google::protobuf::Empty* reply) override {
     logToFile("AppResponse called");
-    json debugJson;
-    debugJson["call"] = "AppResponse";
-    json appResponseRequest;
-    appResponseRequest["node_id"] = request->node_id().c_str();
-    appResponseRequest["request_id"] = boost::lexical_cast<std::string>(request->request_id());
-    appResponseRequest["response"] = request->response().c_str();
-    debugJson["request"] = appResponseRequest;
-    logToFile(debugJson.dump(2));
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status AppGossip(ServerContext* context, const vmproto::AppGossipMsg* request, google::protobuf::Empty* reply) override {
     logToFile("AppGossip called");
-    json debugJson;
-    debugJson["call"] = "AppGossip";
-    json appGossipRequest;
-    appGossipRequest["node_id"] = request->node_id().c_str();
-    appGossipRequest["msg"] = request->msg().c_str();
-    debugJson["request"] = appGossipRequest;
-    logToFile(debugJson.dump(2));
-    
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status Gather(ServerContext* context, const google::protobuf::Empty* request, vmproto::GatherResponse* reply) override {
     logToFile("Gather called");
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status BlockVerify(ServerContext* context, const vmproto::BlockVerifyRequest* request, vmproto::BlockVerifyResponse* reply) override {
     logToFile("BlockVerify called");
-    json debugJson;
-    debugJson["call"] = "BlockVerify";
-    json blockVerifyRequest;
-    blockVerifyRequest["bytes"] = request->bytes().c_str();
-    debugJson["request"] = blockVerifyRequest;
-    logToFile(debugJson.dump(2));
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status BlockAccept(ServerContext* context, const vmproto::BlockAcceptRequest* request, google::protobuf::Empty* reply) override {
     logToFile("BlockAccept called");
-    json debugJson;
-    debugJson["call"] = "BlockAccept";
-    json blockAcceptRequest;
-    blockAcceptRequest["id"] = request->id().c_str();
-    debugJson["request"] = blockAcceptRequest;
-    logToFile(debugJson.dump(2));
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status BlockReject(ServerContext* context, const vmproto::BlockRejectRequest* request, google::protobuf::Empty* reply) override {
     logToFile("BlockReject called");
-    json debugJson;
-    debugJson["call"] = "BlockReject";
-    json blockRejectRequest;
-    blockRejectRequest["id"] = request->id().c_str();
-    debugJson["request"] = blockRejectRequest;
-    logToFile(debugJson.dump(2));
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status GetAncestors(ServerContext* context, const vmproto::GetAncestorsRequest* request, vmproto::GetAncestorsResponse* reply) override {
     logToFile("GetAncestors called");
-    json debugJson;
-    debugJson["call"] = "GetAncestors";
-    json getAncestorsRequest;
-    getAncestorsRequest["blk_id"] = request->blk_id().c_str();
-    getAncestorsRequest["max_blocks_num"] = boost::lexical_cast<std::string>(request->max_blocks_num());
-    getAncestorsRequest["max_blocks_size"] = boost::lexical_cast<std::string>(request->max_blocks_size());
-    getAncestorsRequest["max_blocks_retrival_time"] = boost::lexical_cast<std::string>(request->max_blocks_retrival_time());
-    debugJson["request"] = getAncestorsRequest;
-    logToFile(debugJson.dump(2));
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status BatchedParseBlock(ServerContext* context, const vmproto::BatchedParseBlockRequest* request, vmproto::BatchedParseBlockResponse* reply) override {
     logToFile("BatchedParseBlock called");
-    json debugJson;
-    debugJson["call"] = "BatchedParseBlock";
-    json batchedParseBlockRequest = json::array();
-    for (auto req : request->request()) {
-      json tmp;
-      tmp["bytes"] = req.c_str();
-      batchedParseBlockRequest.push_back(tmp);
-    }
-    debugJson["request"] = batchedParseBlockRequest;
-    logToFile(debugJson.dump(2));
-    
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status VerifyHeightIndex(ServerContext* context, const google::protobuf::Empty* request, vmproto::VerifyHeightIndexResponse* reply) override {
     logToFile("VerifyHeightIndex called");
+    logToFile(request->DebugString());
     return Status::OK;
   }
 
   Status GetBlockIDAtHeight(ServerContext* context, const vmproto::GetBlockIDAtHeightRequest* request, vmproto::GetBlockIDAtHeightResponse* reply) override {
     logToFile("GetBlockIDAtHeight called");
-    json debugJson;
-    debugJson["call"] = "GetBlockIDAtHeight";
-    json getBlockIDAtHeight;
-    getBlockIDAtHeight["height"] = boost::lexical_cast<std::string>(request->height());
-    debugJson["request"] = getBlockIDAtHeight;
+    logToFile(request->DebugString());
     return Status::OK;
   }
 };
