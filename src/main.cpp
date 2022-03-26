9a68dca8f24302cc50dc9c24281118ab87229461
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2022-03-26 16:26:48 +0100

    Add block genesis

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include "main.h"
-
 int main() {
+  // Create genesis block.
+
+  std::string genesisPrevBlock = "0000000000000000000000000000000000000000000000000000000000000000";
+  dev::u256 genesisTimestamp = 0;
+  dev::u256 genesisTxCount = 0;
+  dev::u256 genesisnHeight = 0;
+  dev::u256 genesisBlockData = 0;
+  Block genesis(genesisPrevBlock, genesisTimestamp, genesisTxCount, genesisnHeight, genesisBlockData);
+
+  std::cout << genesis.blockHash() << std::endl;
+  return 0;
   RunServer();
 
   return 0;
