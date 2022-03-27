863912718facc6096504d4492945b9811f2a80bc
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2022-03-27 03:35:19 +0200

    Push server back to same file as main.h (stupid, but work lmao)

--- src/main.h
+++ src/main.h
@@ -23,12 +23,33 @@
 #include <boost/lexical_cast.hpp>
 #include <boost/algorithm/hex.hpp>
 #include <boost/thread.hpp>
+
+#include <boost/asio.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/config.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+
 #include "json.hpp"
 
 #include "block.h"
 #include "utils.h"
 #include "db.h"
-#include "httpServer.h"
 
 using grpc::Server;
 using grpc::ServerAsyncResponseWriter;
@@ -43,6 +64,10 @@ Block genesis("0000000000000000000000000000000000000000000000000000000000000000"
   0,
   ""
 );
+
+class VMServiceImplementation;
+
+void startServer(std::shared_ptr<VMServiceImplementation> service_pointer);
 
 // Logic and data behind the server's behavior.
 class VMServiceImplementation final : public vmproto::VM::Service, public std::enable_shared_from_this<VMServiceImplementation> {
@@ -273,11 +298,214 @@ class VMServiceImplementation final : public vmproto::VM::Service, public std::e
     return Status::OK;
   }
   
+  public:
   void processRPCMessage(std::string message) {
     Utils::logToFile(message);
     return;
   }
 };
+
+template<
+    class Body, class Allocator,
+    class Send>
+void
+handle_request(
+    beast::string_view doc_root,
+    http::request<Body, http::basic_fields<Allocator>>&& req,
+    Send&& send,
+    std::shared_ptr<VMServiceImplementation> service_pointer)
+{
+    // Returns a bad request response
+    auto const bad_request =
+    [&req](beast::string_view why)
+    {
+        http::response<http::string_body> res{http::status::bad_request, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "text/html");
+        res.keep_alive(req.keep_alive());
+        res.body() = std::string(why);
+        res.prepare_payload();
+        return res;
+    };
+
+    // Returns a not found response
+    auto const not_found =
+    [&req](beast::string_view target)
+    {
+        http::response<http::string_body> res{http::status::not_found, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "text/html");
+        res.keep_alive(req.keep_alive());
+        res.body() = "The resource '" + std::string(target) + "' was not found.";
+        res.prepare_payload();
+        return res;
+    };
+
+    // Returns a server error response
+    auto const server_error =
+    [&req](beast::string_view what)
+    {
+        http::response<http::string_body> res{http::status::internal_server_error, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "text/html");
+        res.keep_alive(req.keep_alive());
+        res.body() = "An error occurred: '" + std::string(what) + "'";
+        res.prepare_payload();
+        return res;
+    };
+
+    // Make sure we can handle the method
+
+    // Request path must be absolute and not contain "..".
+    if( req.target().empty() ||
+        req.target()[0] != '/' ||
+        req.target().find("..") != beast::string_view::npos)
+        return send(bad_request("Illegal request-target"));
+
+    std::string request = req.body();
+
+    // Respond to OPTIONS.
+    // there is absolute no fkin documentation over this
+    // someone plz write some :pray:
+    if (req.method() == http::verb::options) {
+        http::response<http::empty_body> res{http::status::ok, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::access_control_allow_origin, "*");
+        res.set(http::field::access_control_allow_methods, "POST, GET");
+        res.set(http::field::access_control_allow_headers, "content-type");
+        res.set(http::field::accept_encoding, "deflate");
+        res.set(http::field::accept_language, "en-US");
+        res.keep_alive(req.keep_alive());
+        return send(std::move(res));
+    }
+    // Respond to POST
+    service_pointer->processRPCMessage(request);
+    http::response<http::string_body> res{http::status::ok, req.version()};
+    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(http::field::content_type, "application/json");
+    res.keep_alive(req.keep_alive());
+    res.prepare_payload();
+    return send(std::move(res));
+}
+
+template<class Stream>
+struct send_lambda
+{
+    Stream& stream_;
+    bool& close_;
+    beast::error_code& ec_;
+
+    explicit
+    send_lambda(
+        Stream& stream,
+        bool& close,
+        beast::error_code& ec)
+        : stream_(stream)
+        , close_(close)
+        , ec_(ec)
+    {
+    }
+
+    template<bool isRequest, class Body, class Fields>
+    void
+    operator()(http::message<isRequest, Body, Fields>&& msg) const
+    {
+        // Determine if we should close the connection after
+        close_ = msg.need_eof();
+
+        // We need the serializer here because the serializer requires
+        // a non-const file_body, and the message oriented version of
+        // http::write only works with const messages.
+        http::serializer<isRequest, Body, Fields> sr{msg};
+        http::write(stream_, sr, ec_);
+    }
+};
+
+void
+fail(beast::error_code ec, char const* what)
+{
+    std::cerr << what << ": " << ec.message() << "\n";
+}
+
+void
+do_session(
+    tcp::socket& socket,
+    std::shared_ptr<std::string const> const& doc_root,
+    std::shared_ptr<VMServiceImplementation> service_pointer)
+{
+    bool close = false;
+    beast::error_code ec;
+
+    // This buffer is required to persist across reads
+    beast::flat_buffer buffer;
+
+    // This lambda is used to send messages
+    send_lambda<tcp::socket> lambda{socket, close, ec};
+
+    for(;;)
+    {
+        // Read a request
+        http::request<http::string_body> req;
+        http::read(socket, buffer, req, ec);
+        if(ec == http::error::end_of_stream)
+            break;
+        if(ec)
+            return fail(ec, "read");
+
+        // Send the response
+        handle_request(*doc_root, std::move(req), lambda, service_pointer);
+        if(ec)
+            return fail(ec, "write");
+        if(close)
+        {
+            // This means we should close the connection, usually because
+            // the response indicated the "Connection: close" semantic.
+            break;
+        }
+    }
+
+    // Send a TCP shutdown
+    socket.shutdown(tcp::socket::shutdown_send, ec);
+
+    // At this point the connection is closed gracefully
+}
+
+void startServer(std::shared_ptr<VMServiceImplementation> service_pointer)
+{
+    try
+    {
+        // Check command line arguments.
+        auto const address = net::ip::make_address("0.0.0.0");
+        auto const port = 80;
+        auto const doc_root = std::make_shared<std::string>("/");
+
+        // The io_context is required for all I/O
+        net::io_context ioc{1};
+
+        // The acceptor receives incoming connections
+        tcp::acceptor acceptor{ioc, {address, port}};
+        for(;;)
+        {
+            // This will receive the new connection
+            tcp::socket socket{ioc};
+
+            // Block until we get a connection
+            acceptor.accept(socket);
+
+            // Launch the session, transferring ownership of the socket
+            std::thread{std::bind(
+                &do_session,
+                std::move(socket),
+                doc_root,
+                service_pointer)}.detach();
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return;
+    }
+}
 
 void RunServer() {
   std::string server_address("0.0.0.0:50051");
