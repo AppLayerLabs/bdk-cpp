f949a7a0d6c78ee44838eb14f65d8efb13a1345f
Jean-Lessa <75625278+Jean-Lessa@users.noreply.github.com>
2022-07-06 15:50:06 -0300

    Join main.h and main.cpp into one

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,24 @@
-#include "main.h"
+#include <iostream>
+#include <vector>
 
-// This stop function is a workaround to allow
-// our subnet to be stopped without the need of
-// AvalancheGo letting it know
-
-// As of 05/07/22, the AvalancheGo daemon
-// Simply kills the subnet, not letting it know through the grpcserver
-// Or any other mean...
+#include "subnet.h"
 
 std::unique_ptr<Subnet> subnet;
 
-void shutdown_handler(int sig) {
-    subnet->stop();
-}
+/**
+ * As of July 5th 2022, the AvalancheGo daemon simply kills the subnet,
+ * not letting it know through the grpcserver or any other means...
+ * This stop function is a workaround to allow our subnet to be stopped
+ * without the need of AvalancheGo letting it know.
+ */
+void shutdown_handler(int sig) { subnet->stop(); }
 
+// Let that good boi run
 int main() {
-    
-    std::signal(SIGINT, SIG_IGN);
-    std::signal(SIGTERM, shutdown_handler);
-    subnet = std::make_unique<Subnet>();
-    subnet->start();
-
-    return 0;
+  std::signal(SIGINT, SIG_IGN);
+  std::signal(SIGTERM, shutdown_handler);
+  subnet = std::make_unique<Subnet>();
+  subnet->start();
+  return 0;
 }
+
