bbab455555da09a3cd463e31a7f8f04123c81801
itamarcps <32653934+itamarcps@users.noreply.github.com>
2022-08-01 07:55:07 -0300

    Reset test folder

--- tests/bridgeFromFuji.sh
+++
@@ -1,6 +0,0 @@
-curl --location --request POST 'localhost:30000/' --header 'Content-Type: application/json' --data-raw '{
- "jsonrpc":"2.0",
- "method":"BridgeFrom",
- "txid":"0xf6a77c53ceaf9c01ee5b76b2ab3eebcdc395c89e39f06be4cabe267909f6ef51",
- "id":1
-}'
