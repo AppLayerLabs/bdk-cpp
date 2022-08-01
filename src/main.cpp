8288bb5a31a6c2991eb0fec0c03ffbe79ba800f7
itamarcps <32653934+itamarcps@users.noreply.github.com>
2022-08-01 13:58:48 -0300

    Reset main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,6 @@ std::unique_ptr<Subnet> subnet;
 
 // Let that good boi run
 int main() {
-
-  std::string myHex = "0xaaaaaa";
-
-  std::string myBytes = Utils::hexToBytes(myHex);
-
-
-  std::cout << dev::toHex(myBytes) << std::endl;;
-
-
-  std::cout << "Trying 7..." << std::endl;;
-
-
-  std::string myHex2  = "0x0aaaaaaa";
-
-  std::string myBytes2 = Utils::hexToBytes(myHex2);
-
-  std::cout << dev::toHex(myBytes2) << std::endl;
-
-
-  return 0;
   std::signal(SIGINT, SIG_IGN);
   //std::signal(SIGTERM, SIG_IGN);
   subnet = std::make_unique<Subnet>();
