0db45202906bec2c60ab3efe043550fcceeed70a
Jean-Lessa <75625278+Jean-Lessa@users.noreply.github.com>
2023-01-06 14:57:27 -0300

    Refactor: add comments on utils headers

--- new_src/utils/hash.h
+++ new_src/utils/hash.h
@@ -17,48 +17,192 @@
 
 #include "string.h"
 
+// TODO: capitalization should be the same for all aliases (PrivKey/PubKey/UPubKey)
 using PrivKey = Hash;
 using Pubkey = FixedStr<33>;
 using UPubkey = FixedStr<65>;
 
+/**
+ * Namespace for abstracting secp256k1 functions.
+ */
 namespace Secp256k1 {
+  /**
+   * Elliptic curve constant (2^256 - 2^32 - 2^9 - 2^8 - 2^7 - 2^6 - 2^4 - 1).
+   */
   static const uint256_t ecConst(
     "115792089237316195423570985008687907852837564279074904382605163141518161494337"
   );
+
+  /**
+   * Recover a public key from a signature and its hash.
+   * @param sig The signature to use for recovery.
+   * @param msg The message hash (in bytes) to use for recovery.
+   * @return The recovered uncompressed public key, or an empty string on failure.
+   */
   UPubkey recover(const Signature& sig, const Hash& msg);
+
+  /**
+   * Create an ECDSA (Elliptic Curve Digital Signature Algorithm) signature.
+   * @param r The first half (32 bytes) of the ECDSA signature.
+   * @param s The second half (32 bytes) of the ECDSA signature.
+   * @param v The recivery id (1 byte).
+   * @returns The full ECDSA signature.
+   */
   Signature makeSig(const uint256_t& r, const uint256_t& s, const uint8_t& v);
+
+  /**
+   * Derive an uncompressed public key from a private one.
+   * @param key The private key to derive from.
+   * @returns The derived uncompressed public key.
+   */
   UPubkey toUPub(const PrivKey& key);
+
+  /**
+   * Derive an uncompressed public key from a compressed public one.
+   * @param key The compressed public key to derive from.
+   * @returns The derived uncompressed public key.
+   */
   UPubkey toUPub(const PubKey& key);
+
+  /**
+   * Derive a compressed public key from a private one.
+   * @param key The private key to derive from.
+   * @return The derived compressed public key.
+   */
   Pubkey toPub(const PrivKey& key);
+
+  /**
+   * Derive an address from a given uncompressed public key.
+   * @param key The uncompressed public key to derive from.
+   * @return The derived address.
+   */
   Address toAddress(const UPubkey& key);
+
+  /**
+   * Derive an address from a given compressed public key.
+   * @param key The compressed public key to derive from.
+   * @return The derived address.
+   */
   Address toAddress(const Pubkey& key);
+
+  /**
+   * Sign a message using a given private key.
+   * @param msg The message to sign.
+   * @param key The private key to use for signing.
+   * @return The signature of the message.
+   */
   Signature sign(const Hash& msg, const PrivKey& key);
+
+  /**
+   * Verify a signature against a given public key and message.
+   * @param msg The message to use for verification.
+   * @param key The uncompressed public key to use for verification.
+   * @param sig The signature to verify.
+   * @return `true` if the signature is verified, `false` otherwise.
+   */
   bool verify(const Hash& msg, const UPubkey& key, const Signature& sig);
 };
 
+/**
+ * Custom hashing implementation for use in `std::unordered_map`.
+ * Based on [this article](https://codeforces.com/blog/entry/62393).
+ * The default `std::unordered_map` implementation uses `uint64_t` hashes,
+ * which makes collisions possible by having many Accounts and distributing them
+ * in a way that they have the same hash across all nodes.
+ * This struct is a workaround for that, it's not perfect because it still uses
+ * `uint64_t`, but it's better than nothing since nodes keeps different hashes.
+ */
 struct SafeHash {
+  /**
+   * Hash a given unsigned integer.
+   * Based on [Sebastiano Vigna's original implementation](http://xorshift.di.unimi.it/splitmix64.c).
+   * @param i A 64-bit unsigned integer.
+   * @returns The hashed data as a 64-bit unsigned integer.
+   */
   uint64_t splitmix(uint64_t i);
+
+  /**
+   * Wrapper for `splitmix()`.
+   * @param i A 64-bit unsigned integer.
+   * @returns The same as `splitmix()`.
+   */
   size_t operator()(uint64_t i);
+
+  /**
+   * Wrapper for `splitmix()`.
+   * @param add An Address object.
+   * @returns The same as `splitmix()`.
+   */
   size_t operator()(const Address& add);
+
+  /**
+   * Wrapper for `splitmix()`.
+   * @param str A regular string.
+   * @returns The same as `splitmix()`.
+   */
   size_t operator()(const std::string& str);
+
+  /**
+   * Wrapper for `splitmix()`.
+   * @param add A Boost ASIO address.
+   * @returns The same as `splitmix()`.
+   */
   size_t operator()(const boost::asio::ip::address add);
+
+  /**
+   * Wrapper for `splitmix()`.
+   * @param ptr A shared pointer to any given type.
+   * @returns The same as `splitmix()`.
+   */
   template <typename T> size_t operator()(const std::shared_ptr<T>& ptr);
+
+  /**
+   * Wrapper for `splitmix()`.
+   * @param str A %FixedStr of any size.
+   * @returns The same as `splitmix()`.
+   */
   template <unsigned N> size_t operator()(const FixedStr<N>& str);
 };
 
+/**
+ * Custom Random Number Generator (RNG) for use in rdPoS.
+ */
 class RandomGen {
   private:
-    Hash seed;
-    std::mutex seedLock;
+    Hash seed;            ///< RNG seed.
+    std::mutex seedLock;  ///< Mutex for managing read/write access to the seed.
   public:
-    typedef uint256_t result_type; // Required for UniformRandomBitGenerator
+    /**
+     * Alias for the result type.
+     * Implemented in conformity with [UniformRandomBitGenerator](https://en.cppreference.com/w/cpp/named_req/UniformRandomBitGenerator).
+     */
+    typedef uint256_t result_type;
+
+    /**
+     * Constructor.
+     * @param seed A random seed for initialization.
+     */
     RandomGen(const Hash& seed) : seed(seed) {};
+
+    /// Getter for `seed`.
     inline Hash& getSeed() { return this->seed; }
+
+    /// Setter for `seed`.
     inline void setSeed(const Hash& seed) { seedLock.lock(); this->seed = seed; seedLock.unlock(); }
+
+    /// Return the maximum numeric limit of a 256-bit unsigned integer.
     inline uint256_t max() { return std::numeric_limits<uint256_t>::max(); }
+
+    /// Return the minimum numeric limit of a 256-bit unsigned integer.
     inline uint256_t min() { return std::numeric_limits<uint256_t>::min(); }
+
+    /**
+     * Shuffle the elements in a vector.
+     * @param v A vector of any given type.
+     */
     template <typename Vector> void shuffle(Vector& v);
-    uint256_t operator()();
+
+    uint256_t operator()(); ///< Generate and return a new random seed.
 };
 
 #endif  // HASH_H
--- new_src/utils/tx.h
+++ new_src/utils/tx.h
@@ -4,71 +4,179 @@
 #include "hash.h"
 #include "strings.h"
 
+/**
+ * Abstraction of a base transaction.
+ * Data inside this type of transaction is never final, it's meant to be
+ * built like it was a struct, then "finalized" by putting it in a %Tx object.
+ */
 class TxData {
   protected:
-    Address to = "";
-    uint256_t value = 0;
-    std::string data = "";
-    uint64_t chainId = 0;
-    uint256_t nonce = 0;
-    uint256_t gas = 0;
-    uint256_t gasPrice = 0;
-    uint256_t v = 0;
-    uint256_t r = 0;
-    uint256_t s = 0;
-    uint256_t nHeight = 0;
-    uint32_t blockIndex = 0;
-    Address from = "";
-    bool callsContract = false;
+    Address to = "";            ///< Receiver address.
+    uint256_t value = 0;        ///< Value in Wei.
+    std::string data = "";      ///< Arbitrary data (e.g. for contracts).
+    uint64_t chainId = 0;       ///< Chain ID where the tx will be broadcast.
+    uint256_t nonce = 0;        ///< Sender address nonce.
+    uint256_t gas = 0;          ///< Gas limit in Wei (e.g. 21000 Wei).
+    uint256_t gasPrice = 0;     ///< Gas price in Wei (usually in Gwei - 1 Gwei = 1000000000 Wei).
+    uint256_t v = 0;            ///< ECDSA recovery ID.
+    uint256_t r = 0;            ///< ECDSA first half.
+    uint256_t s = 0;            ///< ECDSA second half.
+    uint256_t nHeight = 0;      ///< Block height.
+    uint32_t blockIndex = 0;    ///< Index of tx inside the block.
+    Address from = "";          ///< Sender address.
+    bool callsContract = false; ///< Indicates whether the tx calls a contract.
   public:
+    /// Setter for `to`.
     inline void setTo(const Address& to) { this->to = to; }
+
+    /// Setter for `value`.
     inline void setValue(const uint256_t& value) { this->value = value; }
+
+    /// Setter for `data`.
     inline void setData(const std::string& data) { this->data = data; }
+
+    /// Setter for `chainId`.
     inline void setChainId(const uint64_t& chainId) { this->chainId = chainId; }
+
+    /// Setter for `nonce`.
     inline void setNonce(const uint256_t& nonce) { this->nonce = nonce; }
+
+    /// Setter for `gas`.
     inline void setGas(const uint256_t& gas) { this->gas = gas; }
+
+    /// Setter for `gasPrice`.
     inline void setGasPrice(const uint256_t& gasPrice) { this->gasPrice = gasPrice; }
+
+    /// Setter for `v`.
     inline void setV(const uint256_t& v) { this->v = v; }
+
+    /// Setter for `r`.
     inline void setR(const uint256_t& r) { this->r = r; }
+
+    /// Setter for `s`.
     inline void setS(const uint256_t& s) { this->s = s; }
+
+    /// Setter for `nHeight`.
     inline void setNHeight(const uint256_t& nHeight) { this->nHeight = nHeight; }
+
+    /// Setter for `blockIndex`.
     inline void setBlockIndex(const uint32_t& blockIndex) { this->blockIndex = blockIndex; }
+
+    /// Setter for `from`.
     inline void setFrom(const Address& from) { this->from = from; }
+
+    /// Setter for `callsContract`.
     inline void setCallsContract(const bool& callsContract) { this->callsContract = callsContract; }
 };
 
+/**
+ * Abstraction of a finalized transaction.
+ * Data inside this type of transaction is always final and can't be altered.
+ */
 class Tx {
   private:
-    TxData data;
+    TxData data;  ///< Finalized tx data.
   public:
+    /**
+     * Constructor.
+     * @param bytes The raw tx bytes to parse.
+     * @param fromDB If `true`, assumes the tx comes from the database.
+     *               If `false`, assumes the tx comes from RLP.
+     *               DB txs are trusted, as they are already validated and
+     *               included in a block, thus no checks are needed.
+     *               RLP txs are untrusted, as they are not validated or
+     *               included in a block yet, thus they need to be checked
+     *               with secp256k1 (as well as derive the sender address).
+     */
     Tx(const std::string_view& bytes, bool fromDB);
-    Tx(const TxData& data);
-    Tx(const Tx& other);
-    Tx(Tx&& other);
+    Tx(const TxData& data); ///< Copy constructor.
+    Tx(const Tx& other);    ///< Copy constructor.
+    Tx(Tx&& other);         ///< Move constructor.
+
+    /// Getter for `to`.
     inline const Address& getTo() { return this->data.to; }
+
+    /// Getter for `value`.
     inline const uint256_t& getValue() { return this->data.value; }
+
+    /// Getter for `data`.
     inline const std::string& getData() { return this->data.data; }
+
+    /// Getter for `chainId`.
     inline const uint64_t& getChainId() { return this->data.chainId; }
+
+    /// Getter for `nonce`.
     inline const uint256_t& getNonce() { return this->data.nonce; }
+
+    /// Getter for `gas`.
     inline const uint256_t& getGas() { return this->data.gas; }
+
+    /// Getter for `gasPrice`.
     inline const uint256_t& getGasPrice() { return this->data.gasPrice; }
+
+    /// Getter for `v`.
     inline const uint256_t& getV() { return this->data.v; }
+
+    /// Getter for `r`.
     inline const uint256_t& getR() { return this->data.r; }
+
+    /// Getter for `s`.
     inline const uint256_t& getS() { return this->data.s; }
+
+    /// Getter for `nHeight`.
     inline const uint256_t& getNHeight() { return this->data.nHeight; }
+
+    /// Getter for `blockIndex`.
     inline const uint32_t& getBlockIndex() { return this->data.blockIndex; }
+
+    /// Getter for `from`.
     inline const Address& getFrom() { return this->data.from; }
+
+    /// Getter for `callsContract`.
     inline const bool& getCallsContract() { return this->data.callsContract; }
+
+    /// Getter for `v`, but calculates the real ID value based on chainId.
     inline const uint256_t recoverId() {
       return uint256_t(uint8_t(this->data.v - (uint256_t(this->data.chainId) * 2 + 35)));
     }
+
+    /**
+     * Create a SHA3 hash of the transaction. Calls `rlpSerialize()` before hashing.
+     * @return The hash of the transaction, in bytes.
+     */
     Hash hash();
+
+    /**
+     * Serialize the transaction to a string in RLP format.
+     * [EIP-155](https://eips.ethereum.org/EIPS/eip-155) compatible.
+     * @param includeSig If `true`, includes the transaction signature (v/r/s).
+     * @return The serialized transaction.
+     */
     const std::string rlpSerialize(bool includeSig);
+
+    /**
+     * Same as `rlpSerialize()`, but checks if tx has a signature/is verified,
+     * and includes extra content like block index, sender address, if the tx
+     * calls a contract and if it's in a block.
+     */
     const std::string serialize();
+
+    /**
+     * Sign the transaction with a given private key.
+     * @param key The private key to sign with.
+     */
     void sign(const PrivKey& key);
+
+    /// Copy assignment operator.
     Tx& operator=(const Tx& other);
+
+    /// Move assignment operator.
     Tx& operator=(Tx&& other);
+
+    /// Inequality operator. Checks the transaction hash.
     bool operator!=(const Tx& tx) { return this->hash() != tx.hash(); }
+
+    /// Equality operator. Checks the transaction hash.
     bool operator==(const Tx& tx) { return this->hash() == tx.hash(); }
 };
 
