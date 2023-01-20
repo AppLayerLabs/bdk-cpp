58719eebce56c8bab97b1cef48a221f2c0111a05
Jean-Lessa <75625278+Jean-Lessa@users.noreply.github.com>
2023-01-20 19:44:09 -0300

    Refactor: rework Tx, move ecdsa.h to its place

--- new_src/utils/tx.h
+++ new_src/utils/tx.h
@@ -1,71 +1,30 @@
 #ifndef TX_H
 #define TX_H
 
+#include "ecdsa.h"
 #include "strings.h"
+#include "utils.h"
 
 /**
- * Abstraction of a base transaction.
- * Data inside this type of transaction is never final, it's meant to be
- * built like it was a struct, then "finalized" by putting it in a %Tx object.
+ * Abstraction of a block transaction.
+ * All transactions are final, they're defined as such during construction.
  */
-class TxData {
-  protected:
-    Address to = "";            ///< Receiver address.
-    uint256_t value = 0;        ///< Value in Wei.
-    std::string data = "";      ///< Arbitrary data (e.g. for contracts).
-    uint64_t chainId = 0;       ///< Chain ID where the tx will be broadcast.
-    uint256_t nonce = 0;        ///< Sender address nonce.
-    uint256_t gas = 0;          ///< Gas limit in Wei (e.g. 21000 Wei).
-    uint256_t gasPrice = 0;     ///< Gas price in Wei (usually in Gwei - 1 Gwei = 1000000000 Wei).
-    uint256_t v = 0;            ///< ECDSA recovery ID.
-    uint256_t r = 0;            ///< ECDSA first half.
-    uint256_t s = 0;            ///< ECDSA second half.
-    Address from = "";          ///< Sender address.
-  public:
-    /// Setter for `to`.
-    inline void setTo(const Address& to) { this->to = to; }
-
-    /// Setter for `value`.
-    inline void setValue(const uint256_t& value) { this->value = value; }
-
-    /// Setter for `data`.
-    inline void setData(const std::string& data) { this->data = data; }
-
-    /// Setter for `chainId`.
-    inline void setChainId(const uint64_t& chainId) { this->chainId = chainId; }
-
-    /// Setter for `nonce`.
-    inline void setNonce(const uint256_t& nonce) { this->nonce = nonce; }
-
-    /// Setter for `gas`.
-    inline void setGas(const uint256_t& gas) { this->gas = gas; }
-
-    /// Setter for `gasPrice`.
-    inline void setGasPrice(const uint256_t& gasPrice) { this->gasPrice = gasPrice; }
-
-    /// Setter for `v`.
-    inline void setV(const uint256_t& v) { this->v = v; }
-
-    /// Setter for `r`.
-    inline void setR(const uint256_t& r) { this->r = r; }
-
-    /// Setter for `s`.
-    inline void setS(const uint256_t& s) { this->s = s; }
-
-    /// Setter for `from`.
-    inline void setFrom(const Address& from) { this->from = from; }
-};
-
-/**
- * Abstraction of a finalized transaction.
- * Data inside this type of transaction is always final and can't be altered.
- */
-class Tx {
+class TxBlock {
   private:
-    TxData data;  ///< Finalized tx data.
+    Address to;         ///< Receiver address.
+    Address from;       ///< Sender address.
+    std::string data;   ///< Arbitrary data (e.g. for contracts).
+    uint64_t chainId;   ///< Chain ID where the tx will be broadcast.
+    uint256_t nonce;    ///< Sender address nonce.
+    uint256_t value;    ///< Value in Wei.
+    uint256_t gas;      ///< Gas limit in Wei (e.g. 21000 Wei).
+    uint256_t gasPrice; ///< Gas price in Wei (usually in Gwei - 1 Gwei = 1000000000 Wei).
+    uint256_t v;        ///< ECDSA recovery ID.
+    uint256_t r;        ///< ECDSA first half.
+    uint256_t s;        ///< ECDSA second half.
   public:
     /**
-     * Constructor.
+     * Raw constructor.
      * @param bytes The raw tx bytes to parse.
      * @param fromDB If `true`, assumes the tx comes from the database.
      *               If `false`, assumes the tx comes from RLP.
@@ -75,47 +34,95 @@ class Tx {
      *               included in a block yet, thus they need to be checked
      *               with secp256k1 (as well as derive the sender address).
      */
-    Tx(const std::string_view& bytes, bool fromDB);
-    Tx(const TxData& data); ///< Copy constructor.
-    Tx(const Tx& other);    ///< Copy constructor.
-    Tx(Tx&& other);         ///< Move constructor.
+    TxBlock(const std::string_view& bytes, bool fromDB);
+
+    /**
+     * Manual constructor. Leave fields blank ("" or 0) if they're not required.
+     * @param to The receiver address.
+     * @param from The sender address.
+     * @param data The arbitrary data string.
+     * @param chainId The chain ID of the transaction.
+     * @param nonce The nonce of the transaction.
+     * @param value The value of the transaction.
+     * @param gas The gas limit of the transaction.
+     * @param gasPrice The gas price of the transaction.
+     * @param privKey The private key used to sign the transaction.
+     */
+    TxBlock(
+      Address to, Address from, std::string data, uint64_t chainId, uint256_t nonce,
+      uint256_t value, uint256_t gas, uint256_t gasPrice, PrivKey privKey
+    ) : to(to), from(from), data(data), chainId(chainId), nonce(nonce),
+      value(value), gas(gas), gasPrice(gasPrice)
+    {
+      ; // TODO: derivate v/r/s using privKey
+    }
+
+    /// Copy constructor.
+    TxBlock(const TxBlock& other) {
+      this->to = other.to;
+      this->from = other.from;
+      this->data = other.data;
+      this->chainId = other.chainId;
+      this->nonce = other.nonce;
+      this->value = other.value;
+      this->gas = other.gas;
+      this->gasPrice = other.gasPrice;
+      this->v = other.v;
+      this->r = other.r;
+      this->s = other.s;
+    }
+
+    /// Move constructor.
+    TxBlock(TxBlock&& other) noexcept :
+      to(std::move(other.to)),
+      from(std::move(other.from)),
+      data(std::move(other.data)),
+      chainId(std::move(other.chainId)),
+      nonce(std::move(other.nonce)),
+      value(std::move(other.value)),
+      gas(std::move(other.gas)),
+      gasPrice(std::move(other.gasPrice)),
+      v(std::move(other.v)),
+      r(std::move(other.r)),
+      s(std::move(other.s)),
+    {}
 
     /// Getter for `to`.
-    inline const Address& getTo() { return this->data.to; }
-
-    /// Getter for `value`.
-    inline const uint256_t& getValue() { return this->data.value; }
-
-    /// Getter for `data`.
-    inline const std::string& getData() { return this->data.data; }
-
-    /// Getter for `chainId`.
-    inline const uint64_t& getChainId() { return this->data.chainId; }
-
-    /// Getter for `nonce`.
-    inline const uint256_t& getNonce() { return this->data.nonce; }
-
-    /// Getter for `gas`.
-    inline const uint256_t& getGas() { return this->data.gas; }
-
-    /// Getter for `gasPrice`.
-    inline const uint256_t& getGasPrice() { return this->data.gasPrice; }
-
-    /// Getter for `v`.
-    inline const uint256_t& getV() { return this->data.v; }
-
-    /// Getter for `r`.
-    inline const uint256_t& getR() { return this->data.r; }
-
-    /// Getter for `s`.
-    inline const uint256_t& getS() { return this->data.s; }
+    inline const Address& getTo() { return this->to; }
 
     /// Getter for `from`.
-    inline const Address& getFrom() { return this->data.from; }
+    inline const Address& getFrom() { return this->from; }
+
+    /// Getter for `data`.
+    inline const std::string& getData() { return this->data; }
+
+    /// Getter for `chainId`.
+    inline const uint64_t& getChainId() { return this->chainId; }
+
+    /// Getter for `nonce`.
+    inline const uint256_t& getNonce() { return this->nonce; }
+
+    /// Getter for `value`.
+    inline const uint256_t& getValue() { return this->value; }
+
+    /// Getter for `gas`.
+    inline const uint256_t& getGas() { return this->gas; }
+
+    /// Getter for `gasPrice`.
+    inline const uint256_t& getGasPrice() { return this->gasPrice; }
+
+    /// Getter for `v`.
+    inline const uint256_t& getV() { return this->v; }
+
+    /// Getter for `r`.
+    inline const uint256_t& getR() { return this->r; }
+
+    /// Getter for `s`.
+    inline const uint256_t& getS() { return this->s; }
 
     /// Getter for `v`, but calculates the real ID value based on chainId.
     inline const uint256_t recoverId() {
-      return uint256_t(uint8_t(this->data.v - (uint256_t(this->data.chainId) * 2 + 35)));
+      return uint256_t(uint8_t(this->v - (uint256_t(this->chainId) * 2 + 35)));
     }
 
     /**
@@ -127,10 +134,11 @@ class Tx {
     /**
      * Serialize the transaction to a string in RLP format.
      * [EIP-155](https://eips.ethereum.org/EIPS/eip-155) compatible.
-     * @param includeSig If `true`, includes the transaction signature (v/r/s).
+     * @param (optional) includeSig If `true`, includes the transaction signature (v/r/s).
+     *                   Defaults to `true`.
      * @return The serialized transaction.
      */
-    const std::string rlpSerialize(bool includeSig);
+    const std::string rlpSerialize(bool includeSig = true);
 
     /**
      * Same as `rlpSerialize()`, but checks if tx has a signature/is verified,
@@ -139,23 +147,141 @@ class Tx {
      */
     const std::string serialize();
 
-    /**
-     * Sign the transaction with a given private key.
-     * @param key The private key to sign with.
-     */
-    void sign(const PrivKey& key);
-
     /// Copy assignment operator.
-    Tx& operator=(const Tx& other);
+    TxBlock& operator=(const TxBlock& other);
 
     /// Move assignment operator.
-    Tx& operator=(Tx&& other);
-
-    /// Inequality operator. Checks the transaction hash.
-    bool operator!=(const Tx& tx) { return this->hash() != tx.hash(); }
+    TxBlock& operator=(TxBlock&& other);
 
     /// Equality operator. Checks the transaction hash.
-    bool operator==(const Tx& tx) { return this->hash() == tx.hash(); }
+    bool operator==(const TxBlock& tx) { return this->hash() == tx.hash(); }
+
+    /// Inequality operator. Checks the transaction hash.
+    bool operator!=(const TxBlock& tx) { return this->hash() != tx.hash(); }
+};
+
+/**
+ * Abstraction of a Validator transaction.
+ * All transactions are final, they're defined as such during construction.
+ */
+class TxValidator {
+  private:
+    Address from;       ///< Sender address.
+    std::string data;   ///< Arbitrary data (e.g. for contracts).
+    uint64_t chainId;   ///< Chain ID where the tx will be broadcast.
+    uint64_t nHeight;   ///< Block height where the tx will be broadcast.
+    uint256_t v;        ///< ECDSA recovery ID.
+    uint256_t r;        ///< ECDSA first half.
+    uint256_t s;        ///< ECDSA second half.
+  public:
+    /**
+     * Raw constructor.
+     * @param bytes The raw tx bytes to parse.
+     * @param fromDB If `true`, assumes the tx comes from the database.
+     *               If `false`, assumes the tx comes from RLP.
+     *               DB txs are trusted, as they are already validated and
+     *               included in a block, thus no checks are needed.
+     *               RLP txs are untrusted, as they are not validated or
+     *               included in a block yet, thus they need to be checked
+     *               with secp256k1 (as well as derive the sender address).
+     */
+    TxValidator(const std::string_view& bytes, bool fromDB);
+
+    /**
+     * Manual constructor. Leave fields blank ("" or 0) if they're not required.
+     * @param from The sender address.
+     * @param data The arbitrary data string.
+     * @param chainId The chain ID of the transaction.
+     * @param nHeight The block height of the transaction.
+     * @param privKey The private key used to sign the transaction.
+     */
+    TxValidator(
+      Address from, std::string data, uint64_t chainId, uint64_t nHeight, PrivKey privKey
+    ) : from(from), data(data), chainId(chainId), nHeight(nHeight) {
+      ; // TODO: derivate v/r/s using privKey
+    }
+
+    /// Copy constructor.
+    TxValidator(const TxValidator& other) {
+      this->from = other.from;
+      this->data = other.data;
+      this->chainId = other.chainId;
+      this->nHeight = other.nHeight;
+      this->v = other.v;
+      this->r = other.r;
+      this->s = other.s;
+    }
+
+    /// Move constructor.
+    TxValidator(TxValidator&& other) noexcept :
+      from(std::move(other.from)),
+      data(std::move(other.data)),
+      chainId(std::move(other.chainId)),
+      nHeight(std::move(other.nHeight)),
+      v(std::move(other.v)),
+      r(std::move(other.r)),
+      s(std::move(other.s)),
+    {}
+
+    /// Getter for `from`.
+    inline const Address& getFrom() { return this->from; }
+
+    /// Getter for `data`.
+    inline const std::string& getData() { return this->data; }
+
+    /// Getter for `chainId`.
+    inline const uint64_t& getChainId() { return this->chainId; }
+
+    /// Getter for `nHeight`.
+    inline const uint64_t& getNHeight() { return this->nHeight; }
+
+    /// Getter for `v`.
+    inline const uint256_t& getV() { return this->v; }
+
+    /// Getter for `r`.
+    inline const uint256_t& getR() { return this->r; }
+
+    /// Getter for `s`.
+    inline const uint256_t& getS() { return this->s; }
+
+    /// Getter for `v`, but calculates the real ID value based on chainId.
+    inline const uint256_t recoverId() {
+      return uint256_t(uint8_t(this->v - (uint256_t(this->chainId) * 2 + 35)));
+    }
+
+    /**
+     * Create a SHA3 hash of the transaction. Calls `rlpSerialize()` before hashing.
+     * @return The hash of the transaction, in bytes.
+     */
+    Hash hash();
+
+    /**
+     * Serialize the transaction to a string in RLP format.
+     * [EIP-155](https://eips.ethereum.org/EIPS/eip-155) compatible.
+     * @param (optional) includeSig If `true`, includes the transaction signature (v/r/s).
+     *                   Defaults to `true`.
+     * @return The serialized transaction.
+     */
+    const std::string rlpSerialize(bool includeSig = true);
+
+    /**
+     * Same as `rlpSerialize()`, but checks if tx has a signature/is verified,
+     * and includes extra content like block index, sender address, if the tx
+     * calls a contract and if it's in a block.
+     */
+    const std::string serialize();
+
+    /// Copy assignment operator.
+    TxValidator& operator=(const TxValidator& other);
+
+    /// Move assignment operator.
+    TxValidator& operator=(TxValidator&& other);
+
+    /// Equality operator. Checks the transaction hash.
+    bool operator==(const TxValidator& tx) { return this->hash() == tx.hash(); }
+
+    /// Inequality operator. Checks the transaction hash.
+    bool operator!=(const TxValidator& tx) { return this->hash() != tx.hash(); }
 };
 
 #endif  // TX_H
