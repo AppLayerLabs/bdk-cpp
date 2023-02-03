388d0f111fed72ca0c3b3b2b59d9286188d24182
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2023-02-03 10:52:44 -0300

    Add comments back to hex.h

--- new_src/utils/hex.h
+++ new_src/utils/hex.h
@@ -24,48 +24,93 @@ template <typename ElemT> struct HexTo {
 
 class Hex {
   private:
-    std::string hex;
-    std::string_view filter = "0123456789abcdefxABCDEFX";
-    bool strict;
-    bool isHexValid(const std::string_view& v = "") const;
+    std::string hex;                                         ///< Internal string data.
+    std::string_view filter = "0123456789abcdefxABCDEFX";    ///< Filter for hex string
+    bool strict;                                             ///< If true, the _hex will include "0x" prefix
+    bool isHexValid(const std::string_view& v = "") const;   ///< Check if the hex string is valid, if argument empty, check _hex
 
   public:
+    Hex()                                                    ///< Default constructor (empty string)
+
+    /**
+     * Move a Hex object from a hex string "0x1234" or "1234"
+     * @param value hex string
+     * @param strict if true, the _hex will include "0x" prefix
+     */
     Hex(std::string&& value, bool strict = false);
+
+    /**
+     * Build a Hex object from a hex string "0x1234" or "1234"
+     * @param value hex string_view
+     * @param strict if true, the _hex will include "0x" prefix
+     */    
     Hex(const std::string_view& value, bool strict = false);
 
+
+    /**
+     * Build a Hex object from a byte string "\x12\x34" would result in "1234"
+     * @param value bytes string
+     * @param strict if true, the _hex will include "0x" prefix
+     */
     static Hex fromBytes(std::string_view bytes, bool strict = false);
-    static Hex fromString(std::string_view bytes, bool strict = false); // TODO: waiting for implementation
+
+    /**
+     * Build a Hex object from a string "exemple" would result in "6578656d706c65"
+     * @param value bytes string
+     * @param strict if true, the _hex will include "0x" prefix
+     */
     static Hex fromUTF8(std::string_view bytes, bool strict = false);
 
+    /**
+     * Build a bytes string from the Hex object
+     * @return bytes string
+     */
     std::string bytes() const;
 
     /**
-     * Returns the data as std::string, if you need the char collection of bytes
-     * use hex.get().data();
+     * Getter for internal hex string
+     * @return hex string
      */
     inline const std::string& get() const { return this->hex; }
 
-    // TODO: convert to properly sized bytes
+    /**
+     * Get uint256_t equivalent to hex.
+     * @return uint256_t value
+     */
     inline uint256_t getUint() const {
       return boost::lexical_cast<HexTo<uint256_t>>(this->hex);
     }
 
+    /**
+     * Get substr of hex string
+     * @param pos position of the first character to include
+     * @param len number of characters to include
+     * @return hex string (new string object)
+    */
     inline std::string substr(size_t pos = 0, size_t len = std::string::npos) const {
       return this->hex.substr(pos, len);
     }
+
+    /**
+     * Get substr of hex string
+     * @param pos position of the first character to include
+     * @param len number of characters to include
+     * @return hex string (string_view object)
+    */
 
     inline std::string_view substr_view(size_t pos = 0, size_t len = std::string::npos) const {
       return std::string_view(this->hex).substr(pos, len);
     }
 
-    Hex& operator+=(const std::string& hex) {
+    Hex& operator+=(const std::string& hex) { ///< operator+= to add a string () to the hex string
+      // TODO: check if hex string is a valid hex string.
       this->hex += (
         hex[0] == '0' && (hex[1] == 'x' || hex[1] == 'X')
       ) ? hex.substr(2) : hex;
       return *this;
     }
 
-    Hex& operator+=(const Hex& other) {
+    Hex& operator+=(const Hex& other) { ///< operator += to add a hex to another hex.
       this->hex += (
         other.hex[0] == '0' && (other.hex[1] == 'x' || other.hex[1] == 'X')
       ) ? other.hex.substr(2) : other.hex;
