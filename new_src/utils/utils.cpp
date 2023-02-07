3f49881dc383838666ab686ca5b400f8429f034a
itamarcps <32653934+itamarcps@users.noreply.github.com>
2023-02-07 09:26:50 -0300

    Deprecate Utils::toChksum and Utils::isChksum

--- new_src/utils/utils.cpp
+++ new_src/utils/utils.cpp
@@ -332,44 +332,44 @@ void Utils::toUpper(std::string& str)
 //   return boost::lexical_cast<HexTo<uint256_t>>(hex);
 // }
 
-void Utils::toChksum(std::string& str) {
-// Hash requires lowercase address without "0x"
-  if (str.substr(0, 2) == "0x" || str.substr(0, 2) == "0X") {
-    str = str.substr(2);
-  }
-  Utils::toLower(str);
-  Hex hash = Utils::sha3(str).hex();
-  for (int i = 0; i < str.length(); i++) {
-    if (!std::isdigit(str[i])) {  // Only check letters (A-F)
-      // If character hash is 8-F then make it uppercase
-      int nibble = std::stoi(hash.substr(i, 1), nullptr, 16);
-      str[i] = (nibble >= 8) ? std::toupper(str[i]) : std::tolower(str[i]);
-    }
-  }
-  str.insert(0, "0x");
-}
-
-bool Utils::isChksum(const std::string& str) {
-  std::string addCpy = str;
-  Utils::toChksum(addCpy);
-  return (str == addCpy);
-}
-
-bool Utils::isAddress(const std::string& add, bool fromRPC) {
-  if(fromRPC)
-  {
-    if (add[0] == '0' && (add[1] == 'x' || add[1] == 'X')) {
-      if(add.size () != 42) { return false; }
-      if(add.substr(2).find_first_not_of("0123456789abcdefABCDEF") != std::string::npos) { return false; }
-      return true;
-    } else {
-      if (add.size() != 40) { return false; }
-      if (add.find_first_not_of("0123456789abcdefABCDEF") != std::string::npos) { return false; }
-      return true;
-    }
-  }
-  return (add.size() == 20);
-}
+// void Utils::toChksum(std::string& str) {
+// // Hash requires lowercase address without "0x"
+//   if (str.substr(0, 2) == "0x" || str.substr(0, 2) == "0X") {
+//     str = str.substr(2);
+//   }
+//   Utils::toLower(str);
+//   Hex hash = Utils::sha3(str).hex();
+//   for (int i = 0; i < str.length(); i++) {
+//     if (!std::isdigit(str[i])) {  // Only check letters (A-F)
+//       // If character hash is 8-F then make it uppercase
+//       int nibble = std::stoi(hash.substr(i, 1), nullptr, 16);
+//       str[i] = (nibble >= 8) ? std::toupper(str[i]) : std::tolower(str[i]);
+//     }
+//   }
+//   str.insert(0, "0x");
+// }
+// 
+// bool Utils::isChksum(const std::string& str) {
+//   std::string addCpy = str;
+//   Utils::toChksum(addCpy);
+//   return (str == addCpy);
+// }
+// 
+// bool Utils::isAddress(const std::string& add, bool fromRPC) {
+//   if(fromRPC)
+//   {
+//     if (add[0] == '0' && (add[1] == 'x' || add[1] == 'X')) {
+//       if(add.size () != 42) { return false; }
+//       if(add.substr(2).find_first_not_of("0123456789abcdefABCDEF") != std::string::npos) { return false; }
+//       return true;
+//     } else {
+//       if (add.size() != 40) { return false; }
+//       if (add.find_first_not_of("0123456789abcdefABCDEF") != std::string::npos) { return false; }
+//       return true;
+//     }
+//   }
+//   return (add.size() == 20);
+// }
 
 json Utils::readConfigFile() {
   if (!std::filesystem::exists("config.json")) {
