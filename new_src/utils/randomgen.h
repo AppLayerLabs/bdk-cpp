7baa1ee8b84822558cfc8a770dd8b5b90cebb295
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2023-02-09 11:27:15 -0300

    Implement RandomGen tests.

--- new_src/utils/randomgen.h
+++ new_src/utils/randomgen.h
@@ -11,8 +11,9 @@
 #include "strings.h"
 
 /**
- * Custom Random Number Generator (RNG) for use in rdPoS.
+ * Custom Pseudo Random Number Generator (RNG) for use in rdPoS.
  */
+
 class RandomGen {
   private:
     Hash seed;            ///< RNG seed.
@@ -37,16 +38,25 @@ class RandomGen {
     inline void setSeed(const Hash& seed) { seedLock.lock(); this->seed = seed; seedLock.unlock(); }
 
     /// Return the maximum numeric limit of a 256-bit unsigned integer.
-    inline uint256_t max() const { return std::numeric_limits<uint256_t>::max(); }
+    static inline uint256_t max() { return std::numeric_limits<result_type>::max(); }
 
     /// Return the minimum numeric limit of a 256-bit unsigned integer.
-    inline uint256_t min() const { return std::numeric_limits<uint256_t>::min(); }
+    static inline uint256_t min() { return std::numeric_limits<result_type>::min(); }
 
     /**
      * Shuffle the elements in a vector.
      * @param v A vector of any given type.
      */
-    template <typename Vector> void shuffle(Vector& v);
+    template <typename Vector> void shuffle(Vector& v) {
+      this->seedLock.lock();
+      for (uint64_t i = 0; i < v.size(); ++i) {
+        this->seed = Utils::sha3(this->seed.get());
+        //std::cout << this->seed.hex() << std::endl; // Uncomment to print seed
+        uint64_t n = i + this->seed.toUint256() % (v.size() - i);
+        std::swap(v[n], v[i]);
+      }
+      this->seedLock.unlock();
+    }
 
     uint256_t operator()(); ///< Generate and return a new random seed.
 };
