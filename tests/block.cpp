5fa3b56c66597eaa25b10398a0fc91345f595dfd
itamarcps <32653934+itamarcps@users.noreply.github.com>
2023-03-10 13:07:24 -0300

    Remove git conflict in tests/block.cpp

--- tests/block.cpp
+++ tests/block.cpp
@@ -18,11 +18,7 @@ namespace TBlock {
 
       Block reconstructedBlock(newBlock.serializeBlock());
 
-<<<<<<< HEAD
       // Check within reconstructed block
-=======
-      // Check within reconstructed block.
->>>>>>> c5887248bec13b176a650ce95f7317a7f957a5dd
       REQUIRE(reconstructedBlock.getValidatorSig() == Signature(Hex::toBytes("fe3140171d99dac0026928a2b94b3f5a3a3a80cf76feb8c0a1bda78448b0b11057312196cb0ef07049ed5b00ac531fe66e69ab39a6aea2c8104b9852e1d9934c01")));
       REQUIRE(reconstructedBlock.getPrevBlockHash() == Hash(Hex::toBytes("22143e16db549af9ccfd3b746ea4a74421847fa0fe7e0e278626a4e7307ac0f6")));
       REQUIRE(reconstructedBlock.getBlockRandomness() == Hash(Hex::toBytes("0000000000000000000000000000000000000000000000000000000000000000")));
@@ -35,11 +31,7 @@ namespace TBlock {
       REQUIRE(reconstructedBlock.getValidatorPubKey() == UPubKey(Hex::toBytes("04a44addae24f026d4589b7e5943e31970bbd70125c576385e4aedf82660adfcdab4a1aee59e707a297149b18e3bfff46d8fcdabb24c870f44dc5dfd0d7ce82310")));
       REQUIRE(reconstructedBlock.isFinalized() == true);
 
-<<<<<<< HEAD
       // Compare created reconstructed block with created block
-=======
-      // Compare created reconstructed block with created block.
->>>>>>> c5887248bec13b176a650ce95f7317a7f957a5dd
       REQUIRE(reconstructedBlock.getValidatorSig() == newBlock.getValidatorSig());
       REQUIRE(reconstructedBlock.getPrevBlockHash() == newBlock.getPrevBlockHash());
       REQUIRE(reconstructedBlock.getBlockRandomness() == newBlock.getBlockRandomness());
@@ -67,11 +59,7 @@ namespace TBlock {
 
       Block reconstructedBlock(newBlock.serializeBlock());
 
-<<<<<<< HEAD
       // Check within reconstructed block
-=======
-      // Check within reconstructed block.
->>>>>>> c5887248bec13b176a650ce95f7317a7f957a5dd
       REQUIRE(reconstructedBlock.getValidatorSig() == Signature(Hex::toBytes("86ea28b408ac437d3a06449278b696cc8c83b48a6cb421f19e21be6ed47970b516ac0c6d6c81c73c6efa41d622f08c818a3120683d3f8706f864b01d3435932701")));
       REQUIRE(reconstructedBlock.getPrevBlockHash() == Hash(Hex::toBytes("97a5ebd9bbb5e330b0b3c74b9816d595ffb7a04d4a29fb117ea93f8a333b43be")));
       REQUIRE(reconstructedBlock.getBlockRandomness() == Hash(Hex::toBytes("0000000000000000000000000000000000000000000000000000000000000000")));
@@ -83,19 +71,11 @@ namespace TBlock {
       REQUIRE(reconstructedBlock.getTxs().size() == 10);
       REQUIRE(reconstructedBlock.getValidatorPubKey() == UPubKey(Hex::toBytes("041f29fad59ec4befa2691303e125188a54651c97adc6601757fd9d62e331e17c7563a144928b32c7b82ab1d12a82a9ca73c97b5a833ebeb903244d74dfdfa97fc")));
       REQUIRE(reconstructedBlock.isFinalized() == true);
-<<<<<<< HEAD
 
       // Compare transactions with original transactions
       for (uint64_t i = 0; i < 10; i++) REQUIRE(reconstructedBlock.getTxs()[i] == tx);
 
       // Compare created reconstructed block with created block
-=======
-      // Compare transactions with original transactions.
-      for (uint64_t i = 0; i < 10; ++i) {
-        REQUIRE(reconstructedBlock.getTxs()[i] == tx);
-      }
-      // Compare created reconstructed block with created block.
->>>>>>> c5887248bec13b176a650ce95f7317a7f957a5dd
       REQUIRE(reconstructedBlock.getValidatorSig() == newBlock.getValidatorSig());
       REQUIRE(reconstructedBlock.getPrevBlockHash() == newBlock.getPrevBlockHash());
       REQUIRE(reconstructedBlock.getBlockRandomness() == newBlock.getBlockRandomness());
