0e347c457d6b6e6bb59bd6fec9bae847a797fc01
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2023-04-12 12:35:50 -0300

    Remove main() override.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,8 @@
 #include <iostream>
 #include "src/core/blockchain.h"
 #include <filesystem>
-#include "src/contract/variables/safeuint64_t.h"
-#include "src/contract/variables/safeuint256_t.h"
-#include "src/contract/variables/safeaddress.h"
-#include "src/contract/variables/safeunorderedmap.h"
-
 
 int main() {
-  uint256_t i ("1927831865120318940191371489123952378115126713");
-
-  auto newValue = i % 10000;
-
-  std::cout << newValue << std::endl;
-
-  return 0;
-
   /// Local binary path + /blockchain
   std::string blockchainPath = std::filesystem::current_path().string() + std::string("/blockchain");
   Blockchain blockchain(blockchainPath);
