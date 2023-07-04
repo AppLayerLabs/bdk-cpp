b9182157559a24dbb1d0b43754461c632c57cea0
jcarraror <julio.a.carraro@gmail.com>
2023-07-04 13:27:08 -0300

    change abi generator tests to be static

--- tests/contract/contractabigenerator.cpp
+++ tests/contract/contractabigenerator.cpp
@@ -1,40 +1,10 @@
 #include "../../src/libs/catch2/catch_amalgamated.hpp"
 #include "../../src/utils/jsonabi.h"
 #include <filesystem>
-#include <string>
-
-using ContractToTest = std::tuple<ERC20, ERC20Wrapper, NativeWrapper>;
-
-void testABIFiles(const std::string &generatedFilePath, const std::string &testFilePath) {
-    std::ifstream generatedFile(generatedFilePath);
-    std::ifstream testFile(testFilePath);
-
-    if (!generatedFile.is_open() || !testFile.is_open()) {
-        if (!generatedFile.is_open()) {
-            std::cout << "Error: Could not open generatedFile at path " << generatedFilePath << std::endl;
-        }
-        if (!testFile.is_open()) {
-            std::cout << "Error: Could not open testFile at path " << testFilePath << std::endl;
-        }
-        return;
-    }
-
-    std::string generatedFileContent((std::istreambuf_iterator<char>(generatedFile)),
-                                     std::istreambuf_iterator<char>());
-    std::string testFileContent((std::istreambuf_iterator<char>(testFile)),
-                                std::istreambuf_iterator<char>());
-
-    REQUIRE(generatedFileContent == testFileContent);
-
-    generatedFile.close();
-    testFile.close();
-}
-
-
 
 TEST_CASE("ContractABIGenerator helper", "[contract][contractabigenerator]") {
   SECTION("ContractABIGenerator writeContractsToJson") {
-    REQUIRE(JsonAbi::writeContractsToJson<ContractToTest>() == 0);
+    REQUIRE(JsonAbi::writeContractsToJson<ERC20, ERC20Wrapper, NativeWrapper>() == 0);
     REQUIRE(std::filesystem::exists("ABI/ERC20.json"));
     REQUIRE(std::filesystem::exists("ABI/ERC20Wrapper.json"));
     REQUIRE(std::filesystem::exists("ABI/NativeWrapper.json"));
@@ -42,34 +12,308 @@ TEST_CASE("ContractABIGenerator helper", "[contract][contractabigenerator]") {
   }
 
   SECTION("ContractABIGenerator check file content ERC20") {
-    std::string workspace = std::getenv("GITHUB_WORKSPACE") ? std::getenv("GITHUB_WORKSPACE") : ".";
-    std::string generatedFilePath = workspace + "/ABI/ERC20.json";
-    std::string testFilePath = workspace + "/tests/ERC20.json";
-
-    testABIFiles(generatedFilePath, testFilePath);
+    json j;
+    std::ifstream i("ABI/ERC20.json");
+    i >> j;
+    REQUIRE(j.size() == 10);
+    REQUIRE(j[0]["name"] == "");
+    REQUIRE(j[0]["stateMutability"] == "nonpayable");
+    REQUIRE(j[0]["inputs"][0]["internalType"] == "string");
+    REQUIRE(j[0]["inputs"][0]["name"] == "erc20_name");
+    REQUIRE(j[0]["inputs"][0]["type"] == "string");
+    REQUIRE(j[0]["inputs"][1]["internalType"] == "string");
+    REQUIRE(j[0]["inputs"][1]["name"] == "erc20_symbol");
+    REQUIRE(j[0]["inputs"][1]["type"] == "string");
+    REQUIRE(j[0]["inputs"][2]["internalType"] == "uint8");
+    REQUIRE(j[0]["inputs"][2]["name"] == "erc20_decimals");
+    REQUIRE(j[0]["inputs"][2]["type"] == "uint8");
+    REQUIRE(j[0]["inputs"][3]["internalType"] == "uint256");
+    REQUIRE(j[0]["inputs"][3]["name"] == "mintValue");
+    REQUIRE(j[0]["inputs"][3]["type"] == "uint256");
+    REQUIRE(j[1]["name"] == "name");
+    REQUIRE(j[1]["stateMutability"] == "view");
+    REQUIRE(j[1]["type"] == "function");
+    REQUIRE(j[1]["outputs"][0]["internalType"] == "string");
+    REQUIRE(j[1]["outputs"][0]["name"] == "");
+    REQUIRE(j[1]["outputs"][0]["type"] == "string");
+    REQUIRE(j[2]["name"] == "symbol");
+    REQUIRE(j[2]["stateMutability"] == "view");
+    REQUIRE(j[2]["type"] == "function");
+    REQUIRE(j[2]["outputs"][0]["internalType"] == "string");
+    REQUIRE(j[2]["outputs"][0]["name"] == "");
+    REQUIRE(j[2]["outputs"][0]["type"] == "string");
+    REQUIRE(j[3]["name"] == "decimals");
+    REQUIRE(j[3]["stateMutability"] == "view");
+    REQUIRE(j[3]["type"] == "function");
+    REQUIRE(j[3]["outputs"][0]["internalType"] == "uint8");
+    REQUIRE(j[3]["outputs"][0]["name"] == "");
+    REQUIRE(j[3]["outputs"][0]["type"] == "uint8");
+    REQUIRE(j[4]["name"] == "totalSupply");
+    REQUIRE(j[4]["stateMutability"] == "view");
+    REQUIRE(j[4]["type"] == "function");
+    REQUIRE(j[4]["outputs"][0]["internalType"] == "uint256");
+    REQUIRE(j[4]["outputs"][0]["name"] == "");
+    REQUIRE(j[4]["outputs"][0]["type"] == "uint256");
+    REQUIRE(j[5]["name"] == "balanceOf");
+    REQUIRE(j[5]["stateMutability"] == "view");
+    REQUIRE(j[5]["type"] == "function");
+    REQUIRE(j[5]["inputs"][0]["internalType"] == "address");
+    REQUIRE(j[5]["inputs"][0]["name"] == "_owner");
+    REQUIRE(j[5]["inputs"][0]["type"] == "address");
+    REQUIRE(j[5]["outputs"][0]["internalType"] == "uint256");
+    REQUIRE(j[5]["outputs"][0]["name"] == "");
+    REQUIRE(j[5]["outputs"][0]["type"] == "uint256");
+    REQUIRE(j[6]["name"] == "transfer");
+    REQUIRE(j[6]["stateMutability"] == "nonpayable");
+    REQUIRE(j[6]["type"] == "function");
+    REQUIRE(j[6]["inputs"][0]["internalType"] == "address");
+    REQUIRE(j[6]["inputs"][0]["name"] == "_to");
+    REQUIRE(j[6]["inputs"][0]["type"] == "address");
+    REQUIRE(j[6]["inputs"][1]["internalType"] == "uint256");
+    REQUIRE(j[6]["inputs"][1]["name"] == "_value");
+    REQUIRE(j[6]["inputs"][1]["type"] == "uint256");
+    REQUIRE(j[7]["name"] == "approve");
+    REQUIRE(j[7]["stateMutability"] == "nonpayable");
+    REQUIRE(j[7]["type"] == "function");
+    REQUIRE(j[7]["inputs"][0]["internalType"] == "address");
+    REQUIRE(j[7]["inputs"][0]["name"] == "_spender");
+    REQUIRE(j[7]["inputs"][0]["type"] == "address");
+    REQUIRE(j[7]["inputs"][1]["internalType"] == "uint256");
+    REQUIRE(j[7]["inputs"][1]["name"] == "_value");
+    REQUIRE(j[7]["inputs"][1]["type"] == "uint256");
+    REQUIRE(j[8]["name"] == "allowance");
+    REQUIRE(j[8]["stateMutability"] == "view");
+    REQUIRE(j[8]["type"] == "function");
+    REQUIRE(j[8]["inputs"][0]["internalType"] == "address");
+    REQUIRE(j[8]["inputs"][0]["name"] == "_owner");
+    REQUIRE(j[8]["inputs"][0]["type"] == "address");
+    REQUIRE(j[8]["inputs"][1]["internalType"] == "address");
+    REQUIRE(j[8]["inputs"][1]["name"] == "_spender");
+    REQUIRE(j[8]["inputs"][1]["type"] == "address");
+    REQUIRE(j[8]["outputs"][0]["internalType"] == "uint256");
+    REQUIRE(j[8]["outputs"][0]["name"] == "");
+    REQUIRE(j[8]["outputs"][0]["type"] == "uint256");
+    REQUIRE(j[9]["name"] == "transferFrom");
+    REQUIRE(j[9]["stateMutability"] == "nonpayable");
+    REQUIRE(j[9]["type"] == "function");
+    REQUIRE(j[9]["inputs"][0]["internalType"] == "address");
+    REQUIRE(j[9]["inputs"][0]["name"] == "_from");
+    REQUIRE(j[9]["inputs"][0]["type"] == "address");
+    REQUIRE(j[9]["inputs"][1]["internalType"] == "address");
+    REQUIRE(j[9]["inputs"][1]["name"] == "_to");
+    REQUIRE(j[9]["inputs"][1]["type"] == "address");
+    REQUIRE(j[9]["inputs"][2]["internalType"] == "uint256");
+    REQUIRE(j[9]["inputs"][2]["name"] == "_value");
+    REQUIRE(j[9]["inputs"][2]["type"] == "uint256");
   }
 
   SECTION("ContractABIGenerator check file content ERC20Wrapper") {
-    std::string workspace = std::getenv("GITHUB_WORKSPACE") ? std::getenv("GITHUB_WORKSPACE") : ".";
-    std::string generatedFilePath = workspace + "/ABI/ERC20Wrapper.json";
-    std::string testFilePath = workspace + "/tests/ERC20Wrapper.json";
-
-    testABIFiles(generatedFilePath, testFilePath);
-  }
+    json j;
+    std::ifstream i("ABI/ERC20Wrapper.json");
+    i >> j;
+    REQUIRE(j.size() == 6);
+    REQUIRE(j[0]["name"] == "");
+    REQUIRE(j[0]["type"] == "constructor");
+    REQUIRE(j[0]["stateMutability"] == "nonpayable");
+    REQUIRE(j[1]["name"] == "getContractBalance");
+    REQUIRE(j[1]["stateMutability"] == "view");
+    REQUIRE(j[1]["type"] == "function");
+    REQUIRE(j[1]["inputs"][0]["internalType"] == "address");
+    REQUIRE(j[1]["inputs"][0]["name"] == "token");
+    REQUIRE(j[1]["inputs"][0]["type"] == "address");
+    REQUIRE(j[1]["outputs"][0]["internalType"] == "uint256");
+    REQUIRE(j[1]["outputs"][0]["name"] == "");
+    REQUIRE(j[1]["outputs"][0]["type"] == "uint256");
+    REQUIRE(j[2]["name"] == "getUserBalance");
+    REQUIRE(j[2]["stateMutability"] == "view");
+    REQUIRE(j[2]["type"] == "function");
+    REQUIRE(j[2]["inputs"][0]["internalType"] == "address");
+    REQUIRE(j[2]["inputs"][0]["name"] == "token");
+    REQUIRE(j[2]["inputs"][0]["type"] == "address");
+    REQUIRE(j[2]["inputs"][1]["internalType"] == "address");
+    REQUIRE(j[2]["inputs"][1]["name"] == "user");
+    REQUIRE(j[2]["inputs"][1]["type"] == "address");
+    REQUIRE(j[2]["outputs"][0]["internalType"] == "uint256");
+    REQUIRE(j[2]["outputs"][0]["name"] == "");
+    REQUIRE(j[2]["outputs"][0]["type"] == "uint256");
+    REQUIRE(j[3]["name"] == "withdraw");
+    REQUIRE(j[3]["stateMutability"] == "nonpayable");
+    REQUIRE(j[3]["type"] == "function");
+    REQUIRE(j[3]["inputs"][0]["internalType"] == "address");
+    REQUIRE(j[3]["inputs"][0]["name"] == "token");
+    REQUIRE(j[3]["inputs"][0]["type"] == "address");
+    REQUIRE(j[3]["inputs"][1]["internalType"] == "uint256");
+    REQUIRE(j[3]["inputs"][1]["name"] == "value");
+    REQUIRE(j[3]["inputs"][1]["type"] == "uint256");
+    REQUIRE(j[4]["name"] == "transferTo");
+    REQUIRE(j[4]["stateMutability"] == "nonpayable");
+    REQUIRE(j[4]["type"] == "function");
+    REQUIRE(j[4]["inputs"][0]["internalType"] == "address");
+    REQUIRE(j[4]["inputs"][0]["name"] == "token");
+    REQUIRE(j[4]["inputs"][0]["type"] == "address");
+    REQUIRE(j[4]["inputs"][1]["internalType"] == "address");
+    REQUIRE(j[4]["inputs"][1]["name"] == "to");
+    REQUIRE(j[4]["inputs"][1]["type"] == "address");
+    REQUIRE(j[4]["inputs"][2]["internalType"] == "uint256");
+    REQUIRE(j[4]["inputs"][2]["name"] == "value");
+    REQUIRE(j[4]["inputs"][2]["type"] == "uint256");
+    REQUIRE(j[5]["name"] == "deposit");
+    REQUIRE(j[5]["stateMutability"] == "nonpayable");
+    REQUIRE(j[5]["type"] == "function");
+    REQUIRE(j[5]["inputs"][0]["internalType"] == "address");
+    REQUIRE(j[5]["inputs"][0]["name"] == "token");
+    REQUIRE(j[5]["inputs"][0]["type"] == "address");
+    REQUIRE(j[5]["inputs"][1]["internalType"] == "uint256");
+    REQUIRE(j[5]["inputs"][1]["name"] == "value");
+    REQUIRE(j[5]["inputs"][1]["type"] == "uint256");
+}
 
   SECTION("ContractABIGenerator check file content NativeWrapper") {
-    std::string workspace = std::getenv("GITHUB_WORKSPACE") ? std::getenv("GITHUB_WORKSPACE") : ".";
-    std::string generatedFilePath = workspace + "/ABI/NativeWrapper.json";
-    std::string testFilePath = workspace + "/tests/NativeWrapper.json";
-
-    testABIFiles(generatedFilePath, testFilePath);
+    json j;
+    std::ifstream i("ABI/NativeWrapper.json");
+    i >> j;
+    REQUIRE(j.size() == 12);
+    REQUIRE(j[0]["name"] == "");
+    REQUIRE(j[0]["type"] == "constructor");
+    REQUIRE(j[0]["stateMutability"] == "nonpayable");
+    REQUIRE(j[0]["inputs"][0]["internalType"] == "string");
+    REQUIRE(j[0]["inputs"][0]["name"] == "erc20_name");
+    REQUIRE(j[0]["inputs"][0]["type"] == "string");
+    REQUIRE(j[0]["inputs"][1]["internalType"] == "string");
+    REQUIRE(j[0]["inputs"][1]["name"] == "erc20_symbol");
+    REQUIRE(j[0]["inputs"][1]["type"] == "string");
+    REQUIRE(j[0]["inputs"][2]["internalType"] == "uint8");
+    REQUIRE(j[0]["inputs"][2]["name"] == "erc20_decimals");
+    REQUIRE(j[0]["inputs"][2]["type"] == "uint8");
+    REQUIRE(j[1]["name"] == "name");
+    REQUIRE(j[1]["stateMutability"] == "view");
+    REQUIRE(j[1]["type"] == "function");
+    REQUIRE(j[1]["outputs"][0]["internalType"] == "string");
+    REQUIRE(j[1]["outputs"][0]["name"] == "");
+    REQUIRE(j[1]["outputs"][0]["type"] == "string");
+    REQUIRE(j[2]["name"] == "symbol");
+    REQUIRE(j[2]["stateMutability"] == "view");
+    REQUIRE(j[2]["type"] == "function");
+    REQUIRE(j[2]["outputs"][0]["internalType"] == "string");
+    REQUIRE(j[2]["outputs"][0]["name"] == "");
+    REQUIRE(j[2]["outputs"][0]["type"] == "string");
+    REQUIRE(j[3]["name"] == "decimals");
+    REQUIRE(j[3]["stateMutability"] == "view");
+    REQUIRE(j[3]["type"] == "function");
+    REQUIRE(j[3]["outputs"][0]["internalType"] == "uint8");
+    REQUIRE(j[3]["outputs"][0]["name"] == "");
+    REQUIRE(j[3]["outputs"][0]["type"] == "uint8");
+    REQUIRE(j[4]["name"] == "totalSupply");
+    REQUIRE(j[4]["stateMutability"] == "view");
+    REQUIRE(j[4]["type"] == "function");
+    REQUIRE(j[4]["outputs"][0]["internalType"] == "uint256");
+    REQUIRE(j[4]["outputs"][0]["name"] == "");
+    REQUIRE(j[4]["outputs"][0]["type"] == "uint256");
+    REQUIRE(j[5]["name"] == "balanceOf");
+    REQUIRE(j[5]["stateMutability"] == "view");
+    REQUIRE(j[5]["type"] == "function");
+    REQUIRE(j[5]["inputs"][0]["internalType"] == "address");
+    REQUIRE(j[5]["inputs"][0]["name"] == "_owner");
+    REQUIRE(j[5]["inputs"][0]["type"] == "address");
+    REQUIRE(j[5]["outputs"][0]["internalType"] == "uint256");
+    REQUIRE(j[5]["outputs"][0]["name"] == "");
+    REQUIRE(j[5]["outputs"][0]["type"] == "uint256");
+    REQUIRE(j[6]["name"] == "transfer");
+    REQUIRE(j[6]["stateMutability"] == "nonpayable");
+    REQUIRE(j[6]["type"] == "function");
+    REQUIRE(j[6]["inputs"][0]["internalType"] == "address");
+    REQUIRE(j[6]["inputs"][0]["name"] == "_to");
+    REQUIRE(j[6]["inputs"][0]["type"] == "address");
+    REQUIRE(j[6]["inputs"][1]["internalType"] == "uint256");
+    REQUIRE(j[6]["inputs"][1]["name"] == "_value");
+    REQUIRE(j[6]["inputs"][1]["type"] == "uint256");
+    REQUIRE(j[7]["name"] == "approve");
+    REQUIRE(j[7]["stateMutability"] == "nonpayable");
+    REQUIRE(j[7]["type"] == "function");
+    REQUIRE(j[7]["inputs"][0]["internalType"] == "address");
+    REQUIRE(j[7]["inputs"][0]["name"] == "_spender");
+    REQUIRE(j[7]["inputs"][0]["type"] == "address");
+    REQUIRE(j[7]["inputs"][1]["internalType"] == "uint256");
+    REQUIRE(j[7]["inputs"][1]["name"] == "_value");
+    REQUIRE(j[7]["inputs"][1]["type"] == "uint256");
+    REQUIRE(j[8]["name"] == "allowance");
+    REQUIRE(j[8]["stateMutability"] == "view");
+    REQUIRE(j[8]["type"] == "function");
+    REQUIRE(j[8]["inputs"][0]["internalType"] == "address");
+    REQUIRE(j[8]["inputs"][0]["name"] == "_owner");
+    REQUIRE(j[8]["inputs"][0]["type"] == "address");
+    REQUIRE(j[8]["inputs"][1]["internalType"] == "address");
+    REQUIRE(j[8]["inputs"][1]["name"] == "_spender");
+    REQUIRE(j[8]["inputs"][1]["type"] == "address");
+    REQUIRE(j[8]["outputs"][0]["internalType"] == "uint256");
+    REQUIRE(j[8]["outputs"][0]["name"] == "");
+    REQUIRE(j[8]["outputs"][0]["type"] == "uint256");
+    REQUIRE(j[9]["name"] == "transferFrom");
+    REQUIRE(j[9]["stateMutability"] == "nonpayable");
+    REQUIRE(j[9]["type"] == "function");
+    REQUIRE(j[9]["inputs"][0]["internalType"] == "address");
+    REQUIRE(j[9]["inputs"][0]["name"] == "_from");
+    REQUIRE(j[9]["inputs"][0]["type"] == "address");
+    REQUIRE(j[9]["inputs"][1]["internalType"] == "address");
+    REQUIRE(j[9]["inputs"][1]["name"] == "_to");
+    REQUIRE(j[9]["inputs"][1]["type"] == "address");
+    REQUIRE(j[9]["inputs"][2]["internalType"] == "uint256");
+    REQUIRE(j[9]["inputs"][2]["name"] == "_value");
+    REQUIRE(j[9]["inputs"][2]["type"] == "uint256");
+    REQUIRE(j[10]["name"] == "deposit");
+    REQUIRE(j[10]["stateMutability"] == "payable");
+    REQUIRE(j[10]["type"] == "function");
+    REQUIRE(j[11]["name"] == "withdraw");
+    REQUIRE(j[11]["stateMutability"] == "payable");
+    REQUIRE(j[11]["type"] == "function");
+    REQUIRE(j[11]["inputs"][0]["internalType"] == "uint256");
+    REQUIRE(j[11]["inputs"][0]["name"] == "_value");
+    REQUIRE(j[11]["inputs"][0]["type"] == "uint256");
   }
 
   SECTION("ContractABIGenerator check file content ContractManager") {
-    std::string workspace = std::getenv("GITHUB_WORKSPACE") ? std::getenv("GITHUB_WORKSPACE") : ".";
-    std::string generatedFilePath = workspace + "/ABI/ContractManager.json";
-    std::string testFilePath = workspace + "/tests/ContractManager.json";
-
-    testABIFiles(generatedFilePath, testFilePath);
+    json j;
+    std::ifstream i("ABI/ContractManager.json");
+    i >> j;
+    REQUIRE(j.size() == 4); // 3 contracts functions + getDeployedContracts
+    REQUIRE(j[0]["name"] == "createNewERC20Contract");
+    REQUIRE(j[0]["stateMutability"] == "nonpayable");
+    REQUIRE(j[0]["type"] == "function");
+    REQUIRE(j[0]["inputs"][0]["internalType"] == "string");
+    REQUIRE(j[0]["inputs"][0]["name"] == "erc20_name");
+    REQUIRE(j[0]["inputs"][0]["type"] == "string");
+    REQUIRE(j[0]["inputs"][1]["internalType"] == "string");
+    REQUIRE(j[0]["inputs"][1]["name"] == "erc20_symbol");
+    REQUIRE(j[0]["inputs"][1]["type"] == "string");
+    REQUIRE(j[0]["inputs"][2]["internalType"] == "uint8");
+    REQUIRE(j[0]["inputs"][2]["name"] == "erc20_decimals");
+    REQUIRE(j[0]["inputs"][2]["type"] == "uint8");
+    REQUIRE(j[0]["inputs"][3]["internalType"] == "uint256");
+    REQUIRE(j[0]["inputs"][3]["name"] == "mintValue");
+    REQUIRE(j[0]["inputs"][3]["type"] == "uint256");
+    REQUIRE(j[1]["name"] == "createNewERC20WrapperContract");
+    REQUIRE(j[1]["stateMutability"] == "nonpayable");
+    REQUIRE(j[1]["type"] == "function");
+    REQUIRE(j[2]["name"] == "createNewNativeWrapperContract");
+    REQUIRE(j[2]["stateMutability"] == "nonpayable");
+    REQUIRE(j[2]["type"] == "function");
+    REQUIRE(j[2]["inputs"][0]["internalType"] == "string");
+    REQUIRE(j[2]["inputs"][0]["name"] == "erc20_name");
+    REQUIRE(j[2]["inputs"][0]["type"] == "string");
+    REQUIRE(j[2]["inputs"][1]["internalType"] == "string");
+    REQUIRE(j[2]["inputs"][1]["name"] == "erc20_symbol");
+    REQUIRE(j[2]["inputs"][1]["type"] == "string");
+    REQUIRE(j[2]["inputs"][2]["internalType"] == "uint8");
+    REQUIRE(j[2]["inputs"][2]["name"] == "erc20_decimals");
+    REQUIRE(j[2]["inputs"][2]["type"] == "uint8");
+    REQUIRE(j[3]["name"] == "getDeployedContracts");
+    REQUIRE(j[3]["stateMutability"] == "view");
+    REQUIRE(j[3]["type"] == "function");
+    REQUIRE(j[3]["outputs"][0]["internalType"] == "string[]");
+    REQUIRE(j[3]["outputs"][0]["name"] == "");
+    REQUIRE(j[3]["outputs"][0]["type"] == "string[]");
+    REQUIRE(j[3]["outputs"][1]["internalType"] == "address[]");
+    REQUIRE(j[3]["outputs"][1]["name"] == "");
+    REQUIRE(j[3]["outputs"][1]["type"] == "address[]");
   }
 }
