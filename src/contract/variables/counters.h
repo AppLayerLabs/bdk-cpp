ebb279dc2ce06cf6a8c4366e07f08b85650c1904
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2023-09-02 16:31:22 -0300

    Implement Counters Library

---
+++ src/contract/variables/counters.h
@@ -0,0 +1,33 @@
+#ifndef COUNTERS_H
+#define COUNTERS_H
+
+#include "safeint.h"
+
+/// Counters for contracts, roughly based on OpenZeppelin implementation.
+class Counter {
+  private:
+    SafeInt_t<64> value_;
+
+  public:
+    Counter(DynamicContract* contract) : value_(contract) {
+        value_ = 0;
+    }
+
+    uint64_t current() {
+        return value_.get();
+    }
+
+    void increment() {
+        value_ += 1;
+    }
+
+    void decrement() {
+        value_ -= 1;
+    }
+
+    void reset() {
+        value_ = 0;
+    }
+};
+
+#endif // COUNTERS_H
