395e4289a49cad1fd82fe786c345dbc4de711264
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2024-02-24 11:45:49 -0700

    Add more tests ERC721

--- tests/contract/erc721.cpp
+++ tests/contract/erc721.cpp
@@ -35,6 +35,7 @@ namespace TERC721 {
         auto owner = sdk.callViewFunction(ERC721Address, &ERC721Test::ownerOf, uint256_t(i));
         REQUIRE(owner == sdk.getChainOwnerAccount().address);
       }
+      REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::balanceOf, sdk.getChainOwnerAccount().address) == 100);
       REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::totalSupply) == 100);
     }
 
@@ -51,8 +52,8 @@ namespace TERC721 {
       }
       // Check if token Id 0...99 is minted and owned by each respective account
       for (int i = 0; i < 100; ++i) {
-        auto owner = sdk.callViewFunction(ERC721Address, &ERC721Test::ownerOf, uint256_t(i));
-        REQUIRE(owner == accounts[i].address);
+        REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::ownerOf, uint256_t(i)) == accounts[i].address);
+        REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::balanceOf, accounts[i].address) == 1);
       }
       REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::totalSupply) == 100);
     }
@@ -71,8 +72,8 @@ namespace TERC721 {
       // Check if token Id 0...99 is minted and owned by each respective account
       uint64_t tokenId = 0;
       for (int i = 99; i >= 0; i--) {
-        auto owner = sdk.callViewFunction(ERC721Address, &ERC721Test::ownerOf, uint256_t(tokenId++));
-        REQUIRE(owner == accounts[i].address);
+        REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::ownerOf, uint256_t(tokenId++)) == accounts[i].address);
+        REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::balanceOf, accounts[i].address) == 1);
       }
       REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::totalSupply) == 100);
     }
@@ -85,8 +86,8 @@ namespace TERC721 {
       }
       // Check if token Id 0...99 is minted and owned by the chain owner
       for (uint64_t i = 0; i < 100; ++i) {
-        auto owner = sdk.callViewFunction(ERC721Address, &ERC721Test::ownerOf, uint256_t(i));
-        REQUIRE(owner == sdk.getChainOwnerAccount().address);
+        REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::ownerOf, uint256_t(i)) == sdk.getChainOwnerAccount().address);
+        REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::balanceOf, sdk.getChainOwnerAccount().address) == 100);
       }
       REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::totalSupply) == 100);
       for (uint64_t i = 0; i < 100; ++i) {
@@ -97,6 +98,7 @@ namespace TERC721 {
       for (uint64_t i = 0; i < 100; ++i) {
         REQUIRE_THROWS(sdk.callViewFunction(ERC721Address, &ERC721Test::ownerOf, uint256_t(i)));
       }
+      REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::balanceOf, sdk.getChainOwnerAccount().address) == 0);
     }
 
     SECTION("ERC721 Mint 100 Different Address Burn 100 Different Address") {
@@ -112,8 +114,8 @@ namespace TERC721 {
       }
       // Check if token Id 0...99 is minted and owned by each respective account
       for (int i = 0; i < 100; ++i) {
-        auto owner = sdk.callViewFunction(ERC721Address, &ERC721Test::ownerOf, uint256_t(i));
-        REQUIRE(owner == accounts[i].address);
+        REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::ownerOf, uint256_t(i)) == accounts[i].address);
+        REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::balanceOf, accounts[i].address) == 1);
       }
       REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::totalSupply) == 100);
       for (int i = 0; i < 100; ++i) {
@@ -123,6 +125,7 @@ namespace TERC721 {
       // Make sure that ownerOf throws (token does not exist)
       for (uint64_t i = 0; i < 100; ++i) {
         REQUIRE_THROWS(sdk.callViewFunction(ERC721Address, &ERC721Test::ownerOf, uint256_t(i)));
+        REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::balanceOf, accounts[i].address) == 0);
       }
     }
 
@@ -141,6 +144,7 @@ namespace TERC721 {
       for (int i = 0; i < 100; ++i) {
         auto owner = sdk.callViewFunction(ERC721Address, &ERC721Test::ownerOf, uint256_t(i));
         REQUIRE(owner == accounts[i].address);
+        REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::balanceOf, accounts[i].address) == 1);
       }
       REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::totalSupply) == 100);
       // REQUIRE THROW trying to burn without allowance
@@ -154,6 +158,10 @@ namespace TERC721 {
       for (int i = 0; i < 100; ++i) {
         sdk.callFunction(ERC721Address, accounts[i], &ERC721Test::approve, sdk.getChainOwnerAccount().address, uint256_t(i));
       }
+      // Check allowance
+      for (int i = 0; i < 100; ++i) {
+        REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::getApproved, uint256_t(i)) == sdk.getChainOwnerAccount().address);
+      }
       for (int i = 0; i < 100; ++i) {
         sdk.callFunction(ERC721Address, &ERC721Test::burn, uint256_t(i));
       }
@@ -161,6 +169,59 @@ namespace TERC721 {
       // Make sure that ownerOf throws (token does not exist)
       for (uint64_t i = 0; i < 100; ++i) {
         REQUIRE_THROWS(sdk.callViewFunction(ERC721Address, &ERC721Test::ownerOf, uint256_t(i)));
+        REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::balanceOf, accounts[i].address) == 0);
+      }
+      // Make sure allowance doesn't exists anymore
+      for (int i = 0; i < 100; ++i) {
+        REQUIRE_THROWS(sdk.callViewFunction(ERC721Address, &ERC721Test::getApproved, uint256_t(i)) == Address());
+      }
+    }
+
+    SECTION("ERC721 transferFrom with allowance from 100 different accounts") {
+      // Generate 100 different TestAccounts
+      std::vector<TestAccount> accounts;
+      for (int i = 0; i < 100; ++i) {
+        accounts.emplace_back(TestAccount::newRandomAccount());
+      }
+      TestAccount accountToSent = TestAccount::newRandomAccount();
+      SDKTestSuite sdk = SDKTestSuite::createNewEnvironment("testERC721Mint100DifferentAddressBurnWithAllowance", accounts);
+      auto ERC721Address = sdk.deployContract<ERC721Test>(std::string("My Test NFT!"), std::string("NFT"), uint64_t(100));
+      for (int i = 0; i < 100; ++i) {
+        sdk.callFunction(ERC721Address, &ERC721Test::mint, accounts[i].address);
+      }
+      // Check if token Id 0...99 is minted and owned by each respective account
+      for (int i = 0; i < 100; ++i) {
+        auto owner = sdk.callViewFunction(ERC721Address, &ERC721Test::ownerOf, uint256_t(i));
+        REQUIRE(owner == accounts[i].address);
+        REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::balanceOf, accounts[i].address) == 1);
+      }
+      REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::totalSupply) == 100);
+      // REQUIRE THROW for transferFrom without allowance
+      for (int i = 0; i < 100; ++i) {
+        REQUIRE_THROWS(sdk.callFunction(ERC721Address, &ERC721Test::transferFrom, accounts[i].address, accountToSent.address, uint256_t(i)));
+      }
+      // Give allowance
+      for (int i = 0; i < 100; ++i) {
+        sdk.callFunction(ERC721Address, accounts[i], &ERC721Test::approve, sdk.getChainOwnerAccount().address, uint256_t(i));
+      }
+      // Check allowance
+      for (int i = 0; i < 100; ++i) {
+        REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::getApproved, uint256_t(i)) == sdk.getChainOwnerAccount().address);
+      }
+      // Transfer from
+      for (int i = 0; i < 100; ++i) {
+        sdk.callFunction(ERC721Address, &ERC721Test::transferFrom, accounts[i].address, accountToSent.address, uint256_t(i));
+      }
+      // Check if token id 0...99 is owned by accountToSent
+      for (int i = 0; i < 100; ++i) {
+        auto owner = sdk.callViewFunction(ERC721Address, &ERC721Test::ownerOf, uint256_t(i));
+        REQUIRE(owner == accountToSent.address);
+        REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::balanceOf, accountToSent.address) == 100);
+      }
+      REQUIRE(sdk.callViewFunction(ERC721Address, &ERC721Test::totalSupply) == 100);
+      // Make sure that the allowance was removed after the transfer
+      for (int i = 0; i < 100; ++i) {
+        REQUIRE_THROWS(sdk.callViewFunction(ERC721Address, &ERC721Test::getApproved, uint256_t(i)) == Address());
       }
     }
   }
