30122eac38efa1b58dbf8f59e8ecd77188edf5e8
lambdart <lambdart@protonmail.com>
2024-02-27 16:13:15 -0300

    Add options in the Log namespace

--- src/utils/logger.h
+++ src/utils/logger.h
@@ -48,6 +48,7 @@ namespace Log {
   const std::string contractManager = "ContractManager";           ///< String for `ContractManager`.
   const std::string syncer = "Syncer";                             ///< String for `Syncer`.
   const std::string event = "Event";                               ///< String for `Event`.
+  const std::string options = "Options";                           ///< String for `Options`.
 }
 
 /// Class for storing log information.
