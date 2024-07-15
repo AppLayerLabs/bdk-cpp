f6f6b37250576f9164574d4259712601de7a228a
Leonardo Amalaral <leonardo.pedrozo@hotmail.com>
2024-07-15 19:39:02 -0300

    SafeVars: substituted all boolean operations overloads with a single boolean operator

--- src/contract/variables/safeunorderedmap.h
+++ src/contract/variables/safeunorderedmap.h
@@ -614,8 +614,8 @@ template <typename Key, typename T> class SafeUnorderedMap : public SafeBase {
 
     ///@{
     /** Equality operator. Checks only the CURRENT value. */
-    inline bool operator==(const std::unordered_map<Key, T, SafeHash>& other) { return this->value_ == other; }
-    inline bool operator==(const SafeUnorderedMap& other) { return this->value_ == other.get(); }
+    inline bool operator==(const std::unordered_map<Key, T, SafeHash>& other) const { return this->value_ == other; }
+    inline bool operator==(const SafeUnorderedMap& other) const { return this->value_ == other.get(); }
     ///@}
 
     /// Commit the value.
