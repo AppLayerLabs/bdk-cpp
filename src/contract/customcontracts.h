a94236289c63dc88b7f220c0f13e1203251f44b9
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2024-07-17 17:47:26 -0300

    Implement Pebble Contract

--- src/contract/customcontracts.h
+++ src/contract/customcontracts.h
@@ -22,20 +22,20 @@ See the LICENSE.txt file in the project root for more information.
 #include "templates/snailtracer.h"
 #include "templates/snailtraceroptimized.h"
 #include "templates/ownable.h"
-
+#include "templates/pebble.h"
 
 /// Typedef for the blockchain's registered contracts.
 #ifdef BUILD_TESTNET
 /// Typedef for the blockchain's registered contracts in TESTNET mode.
 using ContractTypes = std::tuple<
-  ERC20, NativeWrapper, DEXV2Pair, DEXV2Factory, DEXV2Router02, ERC721
+  ERC20, NativeWrapper, DEXV2Pair, DEXV2Factory, DEXV2Router02, ERC721, Pebble
 >;
 #else
 /// Typedef for the blockchain's registered contracts in normal mode.
 using ContractTypes = std::tuple<
   ERC20, ERC20Wrapper, NativeWrapper, SimpleContract, DEXV2Pair, DEXV2Factory,
   DEXV2Router02, ERC721, ThrowTestA, ThrowTestB, ThrowTestC, ERC721Test, TestThrowVars,
-  RandomnessTest, SnailTracer, SnailTracerOptimized, Ownable
+  RandomnessTest, SnailTracer, SnailTracerOptimized, Pebble
 >;
 #endif
 
---
+++ tests/contract/pebble.cpp
@@ -0,0 +1,48 @@
+/*
+Copyright (c) [2023-2024] [AppLayer Developers]
+
+This software is distributed under the MIT License.
+See the LICENSE.txt file in the project root for more information.
+*/
+
+#include "../../src/libs/catch2/catch_amalgamated.hpp"
+#include "../../src/contract/templates/pebble.h"
+
+#include "../sdktestsuite.hpp"
+
+#include <filesystem>
+
+namespace TPEBBLE {
+  TEST_CASE("Pebble Class", "[contract][pebble]") {
+    SECTION("Pebble creation") {
+      SDKTestSuite sdk = SDKTestSuite::createNewEnvironment("testPebbleCreation");
+      Address pebbleAddr = sdk.deployContract<Pebble>(uint256_t(100000));
+      REQUIRE(sdk.callViewFunction(pebbleAddr, &Pebble::name) == "Pebble");
+      REQUIRE(sdk.callViewFunction(pebbleAddr, &Pebble::symbol) == "PBL");
+      REQUIRE(sdk.callViewFunction(pebbleAddr, &Pebble::totalSupply) == uint256_t(0));
+      REQUIRE(sdk.callViewFunction(pebbleAddr, &Pebble::maxSupply) == uint256_t(100000));
+    }
+    SECTION("Pebble minting") {
+      SDKTestSuite sdk = SDKTestSuite::createNewEnvironment("testPebbleMinting");
+      Address pebbleAddr = sdk.deployContract<Pebble>(uint256_t(100000));
+
+      auto mintTx = sdk.callFunction(pebbleAddr, &Pebble::mintNFT, sdk.getChainOwnerAccount().address);
+
+      auto events = sdk.getEventsEmittedByTxTup(mintTx, &Pebble::MintedNFT);
+      REQUIRE(events.size() == 1);
+      auto event = events[0];
+      REQUIRE(std::get<0>(event) == sdk.getChainOwnerAccount().address);
+      REQUIRE(std::get<1>(event) == uint256_t(0));
+      // Derive the same randomness as the one generated to create the rarity
+      // then check against the rarity inside the event.
+      auto latestBlock = sdk.getStorage().latest();
+      auto latestRandomness = latestBlock->getBlockRandomness().toUint256();
+      auto expectedRarity = Pebble::determineRarity_(latestRandomness);
+      REQUIRE(std::get<2>(event) == expectedRarity);
+      REQUIRE(sdk.callViewFunction(pebbleAddr, &Pebble::totalSupply) == uint256_t(1));
+      REQUIRE(sdk.callViewFunction(pebbleAddr, &Pebble::ownerOf, uint256_t(0)) == sdk.getChainOwnerAccount().address);
+      REQUIRE(sdk.callViewFunction(pebbleAddr, &Pebble::getTokenRarity, uint256_t(0)) == Pebble::rarityToString_(expectedRarity));
+    }
+  }
+}
+
