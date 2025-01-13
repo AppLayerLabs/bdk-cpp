60e4505cdb5ac47d3fc77a4871b43a4d7368ca46
Fabiana Cecin <fcecin@gmail.com>
2025-01-13 09:31:32 -0300

    integrate createcontract.cpp and nativewrapper.cpp tests

---
+++ tests/contract/evm.cpp
@@ -0,0 +1,3 @@
+// NOTE: evm.cpp test has state save/load which is currently unsupported
+//       probably need to rewrite these tests so that they aren't
+//       also doing state serialization/deserialization testing
