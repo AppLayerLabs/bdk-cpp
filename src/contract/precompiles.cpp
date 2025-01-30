762b58831e9db5594d3ac70a56ce8b5bbca1d226
Leonardo Amalaral <leonardo.pedrozo@hotmail.com>
2025-01-30 18:39:59 -0300

    bytes1...bytes32 added with proper abi encoding/decoding, and ripemd160 precompiled added

--- src/contract/precompiles.cpp
+++ src/contract/precompiles.cpp
@@ -2,6 +2,7 @@
 #include "utils/signature.h"
 #include "utils/ecdsa.h"
 #include <openssl/sha.h>
+#include <openssl/ripemd.h>
 
 Address ecrecover(View<Hash> hash, uint8_t v, View<Hash> r, View<Hash> s) {
   if (v == 27) {
@@ -27,5 +28,11 @@ Address ecrecover(View<Hash> hash, uint8_t v, View<Hash> r, View<Hash> s) {
 Hash sha256(View<Bytes> input) {
   Hash output;
   SHA256(input.data(), input.size(), output.data());
+  return output;
+}
+
+Bytes20 ripemd160(View<Bytes> input) {
+  Bytes20 output;
+  RIPEMD160(input.data(), input.size(), output.data()); // TODO: this is deprecated
   return output;
 }
--- src/contract/precompiles.h
+++ src/contract/precompiles.h
@@ -3,9 +3,12 @@
 
 #include "utils/address.h"
 #include "utils/hash.h"
+#include "utils/fixedbytes.h"
 
 Address ecrecover(View<Hash> hash, uint8_t v, View<Hash> r, View<Hash> s);
 
-Hash sha256(View<Bytes> data);
+Hash sha256(View<Bytes> input);
+
+Bytes20 ripemd160(View<Bytes> input);
 
 #endif // BDK_CONTRACT_PRECOMPILES_H
