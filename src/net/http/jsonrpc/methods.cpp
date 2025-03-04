b2b922d32914242b33bbcdcedd4aae0a8b9b6a8f
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2025-03-04 12:55:49 -0300

    Fix eth_getCode

--- src/net/http/jsonrpc/methods.cpp
+++ src/net/http/jsonrpc/methods.cpp
@@ -303,8 +303,7 @@ json eth_getCode(const json& request, const Storage& storage, const State& state
   // Same reasoning as on parseEvmcMessage (Metamask not keeping up)
   // if (!block.isLatest(storage))
   //  throw DynamicException("Only the latest block is supported");
-
-  return Hex::fromBytes(state.getContractCode(address), true).forRPC();
+  return Hex::fromBytes(state.getContractCode(address), true);
 }
 
 json eth_sendRawTransaction(const json& request, uint64_t chainId, State& state, P2P::ManagerNormal& p2p) {
